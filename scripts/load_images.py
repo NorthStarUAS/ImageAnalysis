bc54a9b6bb810b5e77569b1927d7b1e5726fef2c
Curtis L. Olson <curt@aeroag.enet.umn.edu>
2015-08-03 15:59:01 -0500

    Another minimal test script (test loading of all image meta data and building an image list for the project.)

---
+++ scripts/load_images.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+sys.path.insert(0, "/usr/local/opencv-2.4.11/lib/python2.7/site-packages/")
+
+import argparse
+import commands
+import cv2
+import fnmatch
+import os.path
+
+sys.path.append('../lib')
+import ProjectMgr
+
+# for all the images in the project image_dir, load the image meta data
+
+parser = argparse.ArgumentParser(description='Load the project\'s images.')
+parser.add_argument('--project', required=True, help='project directory')
+
+args = parser.parse_args()
+
+proj = ProjectMgr.ProjectMgr(args.project)
+proj.load_image_info()
