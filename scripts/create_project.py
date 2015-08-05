5bb8fb3260dab647fa28ba8671983b634545230b
Curtis L. Olson <curt@aeroag.enet.umn.edu>
2015-08-05 15:15:10 -0500

    Initial revision of several test/debug scripts.

---
+++ scripts/create_project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+sys.path.insert(0, "/usr/local/opencv-2.4.11/lib/python2.7/site-packages/")
+
+import argparse
+import commands
+import cv2
+import fnmatch
+import os.path
+
+sys.path.append('../lib')
+import ProjectMgr
+
+# create an empty project
+
+parser = argparse.ArgumentParser(description='Create an empty project.')
+parser.add_argument('--project', required=True, help='project directory')
+
+args = parser.parse_args()
+
+proj = ProjectMgr.ProjectMgr(args.project)
+proj.save()
---
+++ scripts/detect_features.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import sys
+sys.path.insert(0, "/usr/local/opencv-2.4.11/lib/python2.7/site-packages/")
+
+import argparse
+import commands
+import cv2
+import fnmatch
+import os.path
+
+sys.path.append('../lib')
+import ProjectMgr
+
+# for all the images in the project image_dir, detect features using the
+# specified method and parameters
+
+parser = argparse.ArgumentParser(description='Load the project\'s images.')
+parser.add_argument('--project', required=True, help='project directory')
+parser.add_argument('--detector', default='SIFT',
+                    choices=['SIFT', 'SURF', 'ORB'])
+parser.add_argument('--sift-max-features', default=2000,
+                    help='maximum SIFT features')
+parser.add_argument('--surf-hessian-threshold', default=600,
+                    help='hessian threshold for surf method')
+parser.add_argument('--orb-max-features', default=2000,
+                    help='maximum ORB features')
+parser.add_argument('--grid-detect', default=1,
+                    help='run detect on gridded squares for (maybe) better feature distribution, 4 is a good starting value, only affects ORB method')
+parser.add_argument('--force', action='store_true',
+                    help='force redection of features even if features already exist')
+parser.add_argument('--show', action='store_true',
+                    help='show features as we detect them')
+
+args = parser.parse_args()
+print args
+
+proj = ProjectMgr.ProjectMgr(args.project)
+proj.load_image_info()
+
+detector_params = { 'detector': args.detector,
+                    'sift-max-features': args.sift_max_features,
+                    'surf-hessian-threshold': args.surf_hessian_threshold,
+                    'orb-max-features': args.orb_max_features,
+                    'grid-detect': args.grid_detect }
+proj.set_detector_params(detector_params)
+
+proj.detect_features(force=args.force, show=args.show)
