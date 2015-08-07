3cb670ff77d4c6e6b41807f44b708d1e83a82d32
Curtis L. Olson <curt@aeroag.enet.umn.edu>
2015-08-06 21:10:53 -0500

    initial revision.

---
+++ scripts/5-set-aircraft-poses.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+sys.path.insert(0, "/usr/local/opencv-2.4.11/lib/python2.7/site-packages/")
+
+import argparse
+import commands
+import cv2
+import fnmatch
+import os.path
+
+sys.path.append('../lib')
+import Pose
+import ProjectMgr
+
+# for all the images in the project image_dir, detect features using the
+# specified method and parameters
+
+parser = argparse.ArgumentParser(description='Set the aircraft poses from flight data.')
+parser.add_argument('--project', required=True, help='project directory')
+parser.add_argument('--sentera', help='use the specified sentera image-metadata.txt file')
+
+args = parser.parse_args()
+
+proj = ProjectMgr.ProjectMgr(args.project)
+proj.load_image_info()
+
+if args.sentera != None:
+    Pose.setAircraftPoses(proj, args.sentera)
+    
