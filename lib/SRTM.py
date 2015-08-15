054e1d561362deef380d8d04a2c148a32070736e
Curtis L. Olson <olson126@umn.edu>
2015-08-14 21:58:26 -0500

    Work on interpolation and plotting.

--- lib/SRTM.py
+++ lib/SRTM.py
@@ -10,6 +10,8 @@ import struct
 import urllib
 import zipfile
 
+import navpy
+
 class SRTM():
     def __init__(self, dict_path):
         self.srtm_dict = {}
@@ -68,24 +70,8 @@ class SRTM():
         f.close()
         self.srtm_z = struct.unpack(">1442401H", contents)
 
-        print "Notice: constructing interpolator"
-
-        # The LinearNDInterpolator works well, but is really slow to construct
-        #
-        # srtm_pts = np.zeros((1201*1201, 2))
-        # srtm_vals = np.zeros((1201*1201))
-        # for r in range(0,1201):
-        #     for c in range(0,1201):
-        #         idx = (1201*r)+c
-        #         va = srtm_z[idx]
-        #         if va == 65535 or va < 0 or va > 10000:
-        #             va = 0.0
-        #         x = -94.0 + (float(c) / 1200.0)
-        #         y = 45 + (float(1200-r) / 1200.0)
-        #         z = va
-        #         srtm_pts[idx] = [x, y]
-        #         srtm_vals[idx] = z
-        # self.i = scipy.interpolate.LinearNDInterpolator(srtm_pts, srtm_vals)
+    def make_lla_interpolator(self):
+        print "Notice: constructing lla interpolator"
         
         srtm_pts = np.zeros((1201, 1201))
         for r in range(0,1201):
@@ -98,7 +84,7 @@ class SRTM():
                 srtm_pts[c,1200-r] = z
         x = np.linspace(-94.0, -93.0, 1201)
         y = np.linspace(45.0, 46.0, 1201)
-        self.i = scipy.interpolate.RegularGridInterpolator((x, y), srtm_pts)
+        self.lla_interp = scipy.interpolate.RegularGridInterpolator((x, y), srtm_pts)
         #print self.i([-93.14530573529404, 45.220697421008396])
         #for i in range(20):
         #    x = -94.0 + random.random()
@@ -106,20 +92,55 @@ class SRTM():
         #    lnd = [0] # self.i([x,y])
         #    mif = my_interpolating_function([x,y])
         #    print "lnd=%s, mif=%s" % (lnd, mif)
+
+    # notice: this is *really* slow.  Adding the lla2ned conversion
+    # makes even more slow.  I think what I want to do is build as
+    # many SRTM instances as we need to cover an area, then resample
+    # just the needed area, at some reasonable grid size, convert just
+    # the resampled points to ned, and build the interpolator out of
+    # the subset of points.  A DEM file can cover 60nm in latitude and
+    # 60nm*cos(lat) in the longitude direction.
+    
+    def make_ned_interpolator(self, ref):
+        print "Notice: constructing ned interpolator"
+
+        # The LinearNDInterpolator works well, but is slow to
+        # construct, ned data is not 'regularly' gridded so we need to
+        # use this fancier interpolator
+        ned_pts = np.zeros((1201*1201, 2))
+        ned_vals = np.zeros((1201*1201))
+        for r in range(0,1201):
+            print "%.1f%%" % (float(r)/float(12.01))
+            for c in range(0,1201):
+                idx = (1201*r)+c
+                va = self.srtm_z[idx]
+                if va == 65535 or va < 0 or va > 10000:
+                    va = 0.0
+                lon = -94.0 + (float(c) / 1200.0)
+                lat = 45 + (float(1200-r) / 1200.0)
+                alt = va
+                ned = navpy.lla2ned( lat, lon, alt, ref[0], ref[1], ref[2] )
+                ned_pts[idx] = ned[0:2]
+                ned_vals[idx] = ned[2]
+        self.ned_interp = scipy.interpolate.LinearNDInterpolator(ned_pts, ned_vals)
         
-    def interpolate(self, point_list):
-        return self.i(point_list)
+    def lla_interpolate(self, point_list):
+        return self.lla_interp(point_list)
+
+    def ned_interpolate(self, point_list):
+        return self.ned_interp(point_list)
 
     def plot_raw(self):
         zzz = np.zeros((1201,1201))
         for r in range(0,1201):
             for c in range(0,1201):
                 va=self.srtm_z[(1201*r)+c]
-                if (va==65535 or va<0 or va>2000):
+                if (va==65535 or va<0 or va>10000):
                     va=0.0
                 zzz[r][c]=float(va)
  
-        zz=np.log1p(zzz)
-        imshow(zz, interpolation='bilinear',cmap=cm.gray,alpha=1.0)
+        #zz=np.log1p(zzz)
+        imshow(zzz, interpolation='bilinear',cmap=cm.gray,alpha=1.0)
         grid(False)
         show()
+
