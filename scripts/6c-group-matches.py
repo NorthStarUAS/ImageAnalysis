53a170f061765227fcd27b1ca785c74c56f903c5
Curtis L. Olson <olson126@umn.edu>
2015-08-28 09:38:43 -0500

    Last rev before deleting.
    Former-commit-id: 9c17b1eddbd438ad34e3067786fd8e1f20e33758

--- scripts/6c-group-matches.py
+++ scripts/6c-group-matches.py
@@ -33,35 +33,6 @@ proj.load_matches()
 
 m = Matcher.Matcher()
 
-# remove non-reciprocal matches (this is another filtering technique
-# to remove potential false positives)
-bar = Bar('Removing non-reciprocal matches:', max=len(proj.image_list))
-for i, i1 in enumerate(proj.image_list):
-    for j, i2 in enumerate(proj.image_list):
-        #print "testing %i vs %i" % (i, j)
-        matches = i1.match_list[j]
-        rmatches = i2.match_list[i]
-        before = len(matches)
-        for k, pair in enumerate(matches):
-            rpair = [pair[1], pair[0]]
-            found = False
-            for r in rmatches:
-                if rpair == r:
-                    found = True
-                    break
-            if not found:
-                #print "not found =", rpair
-                matches[k] = [-1, -1]
-        for pair in reversed(matches):
-            if pair == [-1, -1]:
-                matches.remove(pair)
-        after = len(matches)
-        #if before != after:
-        #    print "  (%d vs. %d) matches %d -> %d" % (i, j, before, after)
-    bar.next()
-#    i1.save_matches()
-bar.finish()
-
 # build a list of all 'unique' keypoints and which containting image
 # (image, index)
 match_dict = {}
@@ -69,12 +40,13 @@ for i, i1 in enumerate(proj.image_list):
     for j, matches in enumerate(i1.match_list):
         if j > i:
             for pair in matches:
-                key1 = "%d-%d" % (i, pair[0])
-                key2 = "%d-%d" % (j, pair[1])
-                if key1 in match_dict:
-                    match_dict[key1].append(key2)
+                key = "%d-%d" % (i, pair[0])
+                m1 = [i, pair[0]]
+                m2 = [j, pair[1]]
+                if key in match_dict:
+                    match_dict[key].append(m2)
                 else:
-                    match_dict[key1] = [key1, key2]
+                    match_dict[key] = [m1, m2]
 #print match_dict
 count = 0.0
 sum = 0.0
@@ -85,6 +57,6 @@ if count > 0.1:
     print "total unique features in image set = %d" % count
     print "kp average instances = %.4f" % (sum / count)
 
-f = open("matches.json", 'w')
-json.dump(match_dict, f, indent=2, sort_keys=True)
+f = open(args.project + "/Matches.json", 'w')
+json.dump(match_dict, f, sort_keys=True)
 f.close()
