f8fcb7684e6c1d1048e33ed26ccd9d0bc518370e
Curtis L. Olson <olson126@umn.edu>
2015-09-17 11:20:11 -0500

    Commit current working version so we can fall back to this if we foul it up.
    Former-commit-id: 9cdc9c9d24bcc1f774cc25cbde3122e770fcc66a

--- scripts/7c-sba.py
+++ scripts/7c-sba.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-# 1. Iterate through all the image pairs and triangulate the match points.
-# 2. Set the 3d location of features to triangulated position (possibly
-#    averaged if the feature is included in multiple matches
-# 3. Compute new camera poses with solvePnP() using triangulated point locations
-# 4. Repeat
+# write out the data in a form useful to pass to the sba (demo) program
+
+# it appears camera poses are basically given as [ R | t ] where R is
+# the same R we use throughout and t is the 'tvec'
+
+# todo, run sba and automatically parse output ...
 
 import sys
 sys.path.insert(0, "/usr/local/opencv-2.4.11/lib/python2.7/site-packages/")
@@ -48,26 +49,51 @@ f = open(args.project + "/Matches.json", 'r')
 matches_dict = json.load(f)
 f.close()
 
+# return a quat (of the R matrix) for the given rvec
+def rvec2quat(rvec):
+    R1 = np.array( [[0, 1, 0], [-1, 0, 0], [0, 0, 1]], dtype=float )
+    Rned2cam, jac = cv2.Rodrigues(rvec)
+    Rned2sba = R1.dot(Rned2cam)
+    # make 3x3 rotation matrix into 4x4 homogeneous matrix
+    hIR = np.concatenate( (np.concatenate( (Rned2sba, np.zeros((3,1))),1),
+                           np.mat([0,0,0,1])) )
+    quat = transformations.quaternion_from_matrix(hIR)
+    return quat
+
 # iterate through the image list and build the camera pose dictionary
 # (and a simple list of camera locations for plotting)
 f = open( args.project + '/sba-cams.txt', 'w' )
 for image in proj.image_list:
+    # try #1
     body2cam = image.get_body2cam()
     ned2body = image.get_ned2body()
+    # R1 = np.array( [[0, -1, 0], [-1, 0, 0], [0, 0, -1]], dtype=float )
     Rtotal = body2cam.dot( ned2body )
     q = transformations.quaternion_from_matrix(Rtotal)
-    ned = np.array(image.camera_pose['ned']) / 10000.0
+
+    # try #2
+    rvec, tvec = image.get_proj()
+    # q = rvec2quat(rvec)
+    
+    # try #3
+    # ned2body = image.get_ned2body()
+    # q = transformations.quaternion_from_matrix(ned2body)
+
+    ned = np.array(image.camera_pose['ned']) / 1.0
+    #s = "%.8f %.8f %.8f %.8f %.8f %.8f %.8f\n" % (q[0], q[1], q[2], q[3],
+    #                                              ned[1], ned[0], -ned[2])
     s = "%.8f %.8f %.8f %.8f %.8f %.8f %.8f\n" % (q[0], q[1], q[2], q[3],
-                                                  ned[1], ned[0], -ned[2])
+                                                  tvec[0,0], tvec[1,0], tvec[2,0])
     f.write(s)
+    print "ned=%s tvec=%s" % (ned, np.squeeze(tvec))
 f.close()
 
 # iterate through the matches dictionary to produce a list of matches
 f = open( args.project + '/sba-points.txt', 'w' )
 for key in matches_dict:
     feat = matches_dict[key]
-    ned = np.array(feat['ned']) / 10000.0
-    s = "%.8f %.8f %.8f " % (ned[1], ned[0], -ned[2])
+    ned = np.array(feat['ned']) / 1.0
+    s = "%.8f %.8f %.8f " % (ned[0], ned[1], ned[2])
     f.write(s)
     pts = feat['pts']
     s = "%d " % (len(pts))
