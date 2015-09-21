c22fc2fa2a6f652bfffff3a0270a2193794f8663
Curtis L. Olson <olson126@umn.edu>
2015-09-21 14:32:58 -0500

    Updated to sort by worst offending image and then worst offending feature within each image.
    Former-commit-id: 5c0936fc38d278eaa71ded54d25fb26cb068fae2

--- scripts/7e-mre.py
+++ scripts/7e-mre.py
@@ -9,6 +9,7 @@ sys.path.insert(0, "/usr/local/opencv-2.4.11/lib/python2.7/site-packages/")
 import argparse
 import cv2
 import json
+import math
 import numpy as np
 
 sys.path.append('../lib')
@@ -27,9 +28,52 @@ f = open(args.project + "/Matches.json", 'r')
 matches_dict = json.load(f)
 f.close()
 
-def mean_reprojection_error():
+# image mean reprojection error
+def compute_image_mre(image, cam):
+    image_sum = 0.0
+    image_max = 0.0
+    #scale = float(image.width) / float(camw)
+    #K = cam.get_K(scale)
+    K = cam.get_K()
+    rvec, tvec = image.get_proj()
+    R, jac = cv2.Rodrigues(rvec)
+    PROJ = np.concatenate((R, tvec), axis=1)
+    result_list = []
+    for i, pt in enumerate(image.obj_pts):
+        uvh = K.dot( PROJ.dot( np.hstack((pt, 1.0)) ).T )
+        #print uvh
+        uvh /= uvh[2]
+        #print uvh
+        #print "%s -> %s" % ( image.img_pts[i], [ np.squeeze(uvh[0,0]), np.squeeze(uvh[1,0]) ] )
+        uv = np.array( [ np.squeeze(uvh[0,0]), np.squeeze(uvh[1,0]) ] )
+        dist = np.linalg.norm(np.array(image.img_pts[i]) - uv)
+        #print dist
+        image_sum += dist
+        if dist > image_max:
+            image_max = dist
+        result_list.append( (dist, i) )
+    # sort by worst max error first
+    result_list = sorted(result_list, key=lambda fields: fields[0],
+                         reverse=True)
+
+    # meta stats on error values
+    image_avg = image_sum / len(image.obj_pts)
+    stddev_sum = 0.0
+    for line in result_list:
+        error = line[0]
+        stddev_sum += (image_avg-error)*(image_avg-error)
+    stddev = math.sqrt(stddev_sum / len(image.obj_pts))
+    print "   error avg = %.2f stddev = %.2f" % (image_avg, stddev)
+
+    for line in result_list:
+        if line[0] > image_avg + 3*stddev:
+            print "feat %d err=%.2f" % (line[1], line[0])
+    return image_avg, image_max
+
+# group mean reprojection error
+def compute_group_mre(image_list, cam):
     # start with a clean slate
-    for image in proj.image_list:
+    for image in image_list:
         image.img_pts = []
         image.obj_pts = []
 
@@ -40,37 +84,41 @@ def mean_reprojection_error():
         points = feature_dict['pts']
         ned = matches_dict[key]['ned']
         for p in points:
-            image = proj.image_list[ p[0] ]
+            image = image_list[ p[0] ]
             kp = image.kp_list[ p[1] ]
             image.img_pts.append( kp.pt )
             image.obj_pts.append( ned )
 
-    camw, camh = proj.cam.get_image_params()
-    for image in proj.image_list:
+    camw, camh = cam.get_image_params()
+    result_list = []
+    for i, image in enumerate(image_list):
         print image.name
         if len(image.img_pts) < 4:
             continue
-        image_sum = 0.0
-        image_max = 0.0
-        scale = float(image.width) / float(camw)
-        K = proj.cam.get_K(scale)
-        rvec, tvec = image.get_proj()
-        R, jac = cv2.Rodrigues(rvec)
-        PROJ = np.concatenate((R, tvec), axis=1)
-        for i, pt in enumerate(image.obj_pts):
-            uvh = K.dot( PROJ.dot( np.hstack((pt, 1.0)) ).T )
-            #print uvh
-            uvh /= uvh[2]
-            #print uvh
-            #print "%s -> %s" % ( image.img_pts[i], [ np.squeeze(uvh[0,0]), np.squeeze(uvh[1,0]) ] )
-            uv = np.array( [ np.squeeze(uvh[0,0]), np.squeeze(uvh[1,0]) ] )
-            dist = np.linalg.norm(np.array(image.img_pts[i]) - uv)
-            #print dist
-            image_sum += dist
-            if dist > image_max:
-                image_max = dist
-        print "%s mre = %.2f max = %.2f" % (image.name,
-                                            image_sum / len(image.obj_pts),
-                                            image_max)
+        image_avg, image_max = compute_image_mre(image, cam)
+        result_list.append( (image_avg, image_max, i) )
+    
+    # sort by worst max error first
+    result_list = sorted(result_list, key=lambda fields: fields[0],
+                         reverse=True)
+    sum = 0.0
+    for line in result_list:
+        sum += line[0]
+        print "%s mre avg=%.2f max=%.2f" % (image_list[line[2]].name,
+                                            line[0], line[1])
+    print "Total mean reprojection error = %.4f" % (sum / len(result_list))
+    
+    # meta stats on error values
+    error_avg = math.sqrt(dist2_sum / len(match))
+    stddev_sum = 0.0
+    for line in report_list:
+        error = line[0]
+        stddev_sum += (error_avg-error)*(error_avg-error)
+    stddev = math.sqrt(stddev_sum / len(match))
+    print "   error avg = %.2f stddev = %.2f" % (error_avg, stddev)
+
+    print "%s mre = %.2f max = %.2f" % (image.name,
+                                        image_sum / len(image.obj_pts),
+                                        image_max)
         
-mean_reprojection_error()
+compute_group_mre(proj.image_list, proj.cam)
