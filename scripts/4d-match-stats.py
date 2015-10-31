53eacb01a17e213ada2459c6923487151053cd6b
Curtis L. Olson <curtolson@flightgear.org>
2015-10-31 10:40:15 -0500

    Update the code to work more like my original intent.  The starting image is the one that has the most connections to other images (best redundancy, etc.) The images then are added/ranked by cycle distance proximity. Later placing or ranking imagery with the smaller cycle distance should minimize error creep.

--- scripts/4d-match-stats.py
+++ scripts/4d-match-stats.py
@@ -56,18 +56,23 @@ for key in sorted(image_dict):
     print "%s keypoints = %d" % (key, image_dict[key])
 print "Average matches per connected image = %.1f" % (sum / len(image_dict))
 
-def hasPlacedNeighbor(image, image_list):
+def bestNeighbor(image, image_list):
+    best_cycle_dist = len(image_list) + 1
+    best_index = None
     for i, pairs in enumerate(image.match_list):
         if len(pairs):
             i2 = image_list[i]
-            if i2.placed:
-                return True
-    return False
+            dist = i2.cycle_dist
+            #print "  neighbor check %d = %d" % ( i, dist )
+            if dist >= 0 and dist < best_cycle_dist:
+                best_cycle_dist = dist
+                best_index = i
+    return best_index, best_cycle_dist
 
 def groupByConnections(image_list):
-    # reset the placed flag
+    # reset the cycle distance for all images
     for image in image_list:
-        image.placed = False
+        image.cycle_dist = -1
         
     # compute number of connections per image
     for image in image_list:
@@ -77,38 +82,46 @@ def groupByConnections(image_list):
                 image.connections += 1
         if image.connections > 1:
             print "%s connections: %d" % (image.name, image.connections)
-        
+
     group_list = []
     group = []
     done = False
     while not done:
         done = True
-        maxcon = None
-        maxidx = None
+        best_index = None
+        best_cycle_dist = len(image_list) + 1
         # find an unplaced image with a placed neighbor that has
         # the most connections to other images
         for i, image in enumerate(image_list):
-            if not image.placed and hasPlacedNeighbor(image, image_list) and (maxcon == None or image.connections > maxcon):
-                maxcon = image.connections
-                maxidx = i
-                done = False
-        if maxidx == None:
+            if image.cycle_dist < 0:
+                index, cycle_dist = bestNeighbor(image, image_list)
+                if cycle_dist >= 0 and (cycle_dist+1 < best_cycle_dist):
+                    best_index = i
+                    best_cycle_dist = cycle_dist+1
+                    done = False
+        if best_index == None:
+            #print "Cannot find an unplaced image with a connected neighbor"
             if len(group):
                 # commit the previous group (if it exists)
                 group_list.append(group)
                 # and start a new group
                 group = []
+                cycle_dist = 0
             # now find an unplaced image that has the most connections
-            # to other images
+            # to other images (new cycle start)
+            max_connections = None
+            best_cycle_dist = 0
             for i, image in enumerate(image_list):
-                if not image.placed and (maxcon == None or image.connections > maxcon):
-                    maxcon = image.connections
-                    maxidx = i
-                    done = False
-        if maxidx != None:
-            image = image_list[maxidx]
-            #print "Adding %s (connections = %d)" % (image.name, maxcon)
-            image.placed = True
+                if image.cycle_dist < 0:
+                    if (max_connections == None or image.connections > max_connections):
+                        max_connections = image.connections
+                        best_index = i
+                        done = False
+                        #print " found image %d connections = %d" % (i, max_connections)
+        if best_index != None:
+            image = image_list[best_index]
+            image.cycle_dist = best_cycle_dist
+            #print "Adding %s (cycles = %d)" % (image.name, best_cycle_dist)
             group.append(image)
 
     print "Group (cycles) report:"
@@ -117,7 +130,7 @@ def groupByConnections(image_list):
             continue
         print "group (size=%d):" % (len(group)),
         for image in group:
-            print image.name,
+            print "%s(%d)" % (image.name, image.cycle_dist),
         print ""
 
     return group_list
