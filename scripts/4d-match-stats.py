9b3c925c442bf67a041f2f651ce8878c036c9959
Curtis L. Olson <olson126@umn.edu>
2015-11-04 11:49:37 -0600

    Move code over to lib/Matcher.py

--- scripts/4d-match-stats.py
+++ scripts/4d-match-stats.py
@@ -56,83 +56,10 @@ for key in sorted(image_dict):
     print "%s keypoints = %d" % (key, image_dict[key])
 print "Average matches per connected image = %.1f" % (sum / len(image_dict))
 
-def bestNeighbor(image, image_list):
-    best_cycle_dist = len(image_list) + 1
-    best_index = None
-    for i, pairs in enumerate(image.match_list):
-        if len(pairs):
-            i2 = image_list[i]
-            dist = i2.cycle_dist
-            #print "  neighbor check %d = %d" % ( i, dist )
-            if dist >= 0 and dist < best_cycle_dist:
-                best_cycle_dist = dist
-                best_index = i
-    return best_index, best_cycle_dist
+Matcher.groupByConnections(proj.image_list)
 
-def groupByConnections(image_list):
-    # reset the cycle distance for all images
-    for image in image_list:
-        image.cycle_dist = -1
-        
-    # compute number of connections per image
-    for image in image_list:
-        image.connections = 0
-        for pairs in image.match_list:
-            if len(pairs) >= 8:
-                image.connections += 1
-        if image.connections > 1:
-            print "%s connections: %d" % (image.name, image.connections)
+# save the results
+for image in proj.image_list:
+    image.save_meta()
 
-    group_list = []
-    group = []
-    done = False
-    while not done:
-        done = True
-        best_index = None
-        best_cycle_dist = len(image_list) + 1
-        # find an unplaced image with a placed neighbor that has
-        # the most connections to other images
-        for i, image in enumerate(image_list):
-            if image.cycle_dist < 0:
-                index, cycle_dist = bestNeighbor(image, image_list)
-                if cycle_dist >= 0 and (cycle_dist+1 < best_cycle_dist):
-                    best_index = i
-                    best_cycle_dist = cycle_dist+1
-                    done = False
-        if best_index == None:
-            #print "Cannot find an unplaced image with a connected neighbor"
-            if len(group):
-                # commit the previous group (if it exists)
-                group_list.append(group)
-                # and start a new group
-                group = []
-                cycle_dist = 0
-            # now find an unplaced image that has the most connections
-            # to other images (new cycle start)
-            max_connections = None
-            best_cycle_dist = 0
-            for i, image in enumerate(image_list):
-                if image.cycle_dist < 0:
-                    if (max_connections == None or image.connections > max_connections):
-                        max_connections = image.connections
-                        best_index = i
-                        done = False
-                        #print " found image %d connections = %d" % (i, max_connections)
-        if best_index != None:
-            image = image_list[best_index]
-            image.cycle_dist = best_cycle_dist
-            #print "Adding %s (cycles = %d)" % (image.name, best_cycle_dist)
-            group.append(image)
-
-    print "Group (cycles) report:"
-    for group in group_list:
-        if len(group) < 2:
-            continue
-        print "group (size=%d):" % (len(group)),
-        for image in group:
-            print "%s(%d)" % (image.name, image.cycle_dist),
-        print ""
-
-    return group_list
-
-groupByConnections(proj.image_list)
+Matcher.buildConnectionDetail(proj.image_list, matches_dict)
