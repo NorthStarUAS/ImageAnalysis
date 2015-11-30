b02abc1ea246b94770f77ce4999e7f3e3cc1112d
Curtis L. Olson <curtolson@flightgear.org>
2015-11-30 06:48:59 -0600

    Add support for aura flight logs. Add experimental support for correlating movie translation vector to linked axes rates.  (If the camera is pointed along one of the aircraft's orthogonal axes, then camera roll will correspond to the rate gyro along that axis, and tranlation (x, y) will correspond to the other two axes with some constant gain related to field of view.

--- movie/correlate1.py
+++ movie/correlate1.py
@@ -9,9 +9,9 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 
 parser = argparse.ArgumentParser(description='correlate movie data to flight data.')
 parser.add_argument('--movie-log', required=True, help='movie data file')
-parser.add_argument('--movie-invert-rot', action='store_true', help='(tmp) invert movie log rotation direction.')
 parser.add_argument('--resample-hz', type=float, default=30.0, help='resample rate (hz)')
-parser.add_argument('--flight-log', required=True, help='APM tlog converted to csv')
+parser.add_argument('--apm-log', help='APM tlog converted to csv')
+parser.add_argument('--aura-log', help='Aura imu.txt file')
 args = parser.parse_args()
 
 r2d = 180.0 / math.pi
@@ -26,61 +26,76 @@ with open(args.movie_log, 'rb') as f:
     for line in f:
         movie.append( line.rstrip().split() )
 
-# load flight log
 flight = []
 last_time = -1
-with open(args.flight_log, 'rb') as f:
-    for line in f:
-        if re.search('yawspeed', line):
-            tokens = line.rstrip().split(',')
-            timestamp = float(tokens[9])/1000.0
+
+if args.apm_log:
+    # load APM flight log
+    with open(args.apm_log, 'rb') as f:
+        for line in f:
+            if re.search('yawspeed', line):
+                tokens = line.rstrip().split(',')
+                timestamp = float(tokens[9])/1000.0
+                if timestamp > last_time:
+                    flight.append( [float(tokens[9])/1000.0, float(tokens[17]),
+                                    float(tokens[19]), float(tokens[21])] )
+                    last_time = timestamp
+                else:
+                    print "ERROR: time went backwards:", timestamp, last_time
+elif args.aura_log:
+    # load Aura flight log
+    with open(args.aura_log, 'rb') as f:
+        for line in f:
+            tokens = line.rstrip().split()
+            timestamp = float(tokens[0])
             if timestamp > last_time:
-                flight.append( [float(tokens[9])/1000.0, float(tokens[17]),
-                                float(tokens[19]), float(tokens[21])] )
+                flight.append( [tokens[0], tokens[1], tokens[2], tokens[3]] )
                 last_time = timestamp
             else:
                 print "ERROR: time went backwards:", timestamp, last_time
-print np.array(flight)
-
+else:
+    print "No flight log specified, cannot continue."
+    quit()
+    
 # resample movie data
 movie = np.array(movie, dtype=float)
 movie_interp = []
 x = movie[:,0]
-if args.movie_invert_rot:
-    y = -movie[:,2]
-else:
-    y = movie[:,2]
-spl = InterpolatedUnivariateSpline(x, y)
+movie_spl_roll = InterpolatedUnivariateSpline(x, movie[:,2])
+movie_spl_pitch = InterpolatedUnivariateSpline(x, movie[:,3])
+movie_spl_yaw = InterpolatedUnivariateSpline(x, movie[:,4])
 xmin = x.min()
 xmax = x.max()
 print "movie range = %.3f - %.3f (%.3f)" % (xmin, xmax, xmax-xmin)
 time = xmax - xmin
 for x in np.linspace(xmin, xmax, time*args.resample_hz):
-    movie_interp.append( [x, spl(x)] )
+    movie_interp.append( [x, movie_spl_roll(x)] )
 print "movie len:", len(movie_interp)
 
 # resample flight data
 flight = np.array(flight, dtype=float)
 flight_interp = []
 x = flight[:,0]
-y = flight[:,3]
-spl = InterpolatedUnivariateSpline(x, y)
+if args.apm_log:
+    print "Fixme: pick correct flight log axes"
+    quit()
+flight_spl_roll = InterpolatedUnivariateSpline(x, flight[:,1])
+flight_spl_pitch = InterpolatedUnivariateSpline(x, flight[:,2])
+flight_spl_yaw = InterpolatedUnivariateSpline(x, flight[:,3])
 xmin = x.min()
 xmax = x.max()
 print "flight range = %.3f - %.3f (%.3f)" % (xmin, xmax, xmax-xmin)
 time = xmax - xmin
 for x in np.linspace(xmin, xmax, time*args.resample_hz):
-    flight_interp.append( [x, spl(x)] )
-    #print x, spl(x)
+    flight_interp.append( [x, flight_spl_roll(x)] )
 print "flight len:", len(flight_interp)
 
+# compute best correlation between movie and flight data logs
 movie_interp = np.array(movie_interp, dtype=float)
 flight_interp = np.array(flight_interp, dtype=float)
 ycorr = np.correlate(movie_interp[:,1], flight_interp[:,1], mode='full')
-print "ycorr size=", ycorr.size
-#for y in ycorr:
-#    print y
 
+# display some stats/info
 max_index = np.argmax(ycorr)
 print "max index:", max_index
 shift = np.argmax(ycorr) - len(flight_interp)
@@ -90,16 +105,59 @@ print "start time diff:", start_diff
 time_shift = start_diff - (shift/args.resample_hz)
 print "movie time shift:", time_shift
 
+# estimate  tx, ty vs. r, q multiplier
+tmin = np.amax( [np.amin(movie_interp[:,0]) + time_shift,
+                np.amin(flight_interp[:,0]) ] )
+tmax = np.amin( [np.amax(movie_interp[:,0]) + time_shift,
+                np.amax(flight_interp[:,0]) ] )
+print "overlap range (flight sec):", tmin, " - ", tmax
+
+mqsum = 0.0
+fqsum = 0.0
+mrsum = 0.0
+frsum = 0.0
+count = 0
+qratio = 1.0
+for x in np.linspace(tmin, tmax, time*args.resample_hz):
+    mqsum += abs(movie_spl_pitch(x-time_shift))
+    mrsum += abs(movie_spl_yaw(x-time_shift))
+    fqsum += abs(flight_spl_pitch(x))
+    frsum += abs(flight_spl_yaw(x))
+if fqsum > 0.001:
+    qratio = mqsum / fqsum
+if mrsum > 0.001:
+    rratio = -mrsum / frsum
+print "pitch ratio:", qratio
+print "yaw ratio:", rratio
+
 plt.figure(1)
-plt.ylabel('yaw rate (deg per sec)')
+plt.ylabel('roll rate (deg per sec)')
 plt.xlabel('flight time (sec)')
 plt.plot(movie_interp[:,0] + time_shift, movie_interp[:,1]*r2d, label='estimate from flight movie')
-plt.plot(flight_interp[:,0], flight_interp[:,1]*r2d, label='iris flight log')
+plt.plot(flight_interp[:,0], flight_interp[:,1]*r2d, label='flight data log')
 #plt.plot(movie_interp[:,1])
 #plt.plot(flight_interp[:,1])
 plt.legend()
 
 plt.figure(2)
 plt.plot(ycorr)
+
+plt.figure(3)
+plt.ylabel('pitch rate (deg per sec)')
+plt.xlabel('flight time (sec)')
+plt.plot(movie[:,0] + time_shift, (movie[:,3]/qratio)*r2d, label='estimate from flight movie')
+plt.plot(flight[:,0], flight[:,2]*r2d, label='flight data log')
+#plt.plot(movie_interp[:,1])
+#plt.plot(flight_interp[:,1])
+plt.legend()
+
+plt.figure(4)
+plt.ylabel('yaw rate (deg per sec)')
+plt.xlabel('flight time (sec)')
+plt.plot(movie[:,0] + time_shift, (movie[:,4]/rratio)*r2d, label='estimate from flight movie')
+plt.plot(flight[:,0], flight[:,3]*r2d, label='flight data log')
+#plt.plot(movie_interp[:,1])
+#plt.plot(flight_interp[:,1])
+plt.legend()
 
 plt.show()
