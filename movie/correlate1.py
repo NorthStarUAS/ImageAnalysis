58b5e532d34798c414b11600442b357193fb99e1
Curtis L. Olson <olson126@umn.edu>
2015-12-04 14:29:37 -0600

    Lots of updates and can now extract and geotag movie frames.

--- movie/correlate1.py
+++ movie/correlate1.py
@@ -1,20 +1,71 @@
 #!/usr/bin/python
 
+# find our custom built opencv first
+import sys
+sys.path.insert(0, "/usr/local/opencv-2.4.11/lib/python2.7/site-packages/")
+
 import argparse
+import cv2
 import math
+import fractions
 from matplotlib import pyplot as plt 
 import numpy as np
+import os
+import pyexiv2
 import re
 from scipy.interpolate import InterpolatedUnivariateSpline
 
 parser = argparse.ArgumentParser(description='correlate movie data to flight data.')
-parser.add_argument('--movie-log', required=True, help='movie data file')
+parser.add_argument('--movie', required=False, help='original movie if extracting frames')
+parser.add_argument('--movie-log', required=True, help='movie log file')
 parser.add_argument('--resample-hz', type=float, default=30.0, help='resample rate (hz)')
 parser.add_argument('--apm-log', help='APM tlog converted to csv')
 parser.add_argument('--aura-log', help='Aura imu.txt file')
 args = parser.parse_args()
 
 r2d = 180.0 / math.pi
+counter = 0
+stop_count = 0
+
+class Fraction(fractions.Fraction):
+    """Only create Fractions from floats.
+
+    >>> Fraction(0.3)
+    Fraction(3, 10)
+    >>> Fraction(1.1)
+    Fraction(11, 10)
+    """
+
+    def __new__(cls, value, ignore=None):
+        """Should be compatible with Python 2.6, though untested."""
+        return fractions.Fraction.from_float(value).limit_denominator(99999)
+
+def dms_to_decimal(degrees, minutes, seconds, sign=' '):
+    """Convert degrees, minutes, seconds into decimal degrees.
+
+    >>> dms_to_decimal(10, 10, 10)
+    10.169444444444444
+    >>> dms_to_decimal(8, 9, 10, 'S')
+    -8.152777777777779
+    """
+    return (-1 if sign[0] in 'SWsw' else 1) * (
+        float(degrees)        +
+        float(minutes) / 60   +
+        float(seconds) / 3600
+    )
+
+
+def decimal_to_dms(decimal):
+    """Convert decimal degrees into degrees, minutes, seconds.
+
+    >>> decimal_to_dms(50.445891)
+    [Fraction(50, 1), Fraction(26, 1), Fraction(113019, 2500)]
+    >>> decimal_to_dms(-125.976893)
+    [Fraction(125, 1), Fraction(58, 1), Fraction(92037, 2500)]
+    """
+    remainder, degrees = math.modf(abs(decimal))
+    remainder, minutes = math.modf(remainder * 60)
+    return [Fraction(n) for n in (degrees, minutes, remainder * 60)]
 
 if args.resample_hz <= 0.001:
     print "Resample rate (hz) needs to be greater than zero."
@@ -94,13 +145,16 @@ print "movie len:", len(movie_interp)
 flight_imu = np.array(flight_imu, dtype=float)
 flight_interp = []
 x = flight_imu[:,0]
-flight_spl_roll = InterpolatedUnivariateSpline(x, flight_imu[:,1])
-flight_spl_pitch = InterpolatedUnivariateSpline(x, flight_imu[:,2])
-flight_spl_yaw = InterpolatedUnivariateSpline(x, flight_imu[:,3])
+flight_imu_p = InterpolatedUnivariateSpline(x, flight_imu[:,1])
+flight_imu_q = InterpolatedUnivariateSpline(x, flight_imu[:,2])
+flight_imu_r = InterpolatedUnivariateSpline(x, flight_imu[:,3])
+flight_imu_roll = InterpolatedUnivariateSpline(x, flight_imu[:,4])
+flight_imu_pitch = InterpolatedUnivariateSpline(x, flight_imu[:,5])
+flight_imu_yaw = InterpolatedUnivariateSpline(x, flight_imu[:,6])
 if args.apm_log:
-    y_spline = flight_spl_yaw
+    y_spline = flight_imu_r
 elif args.aura_log:
-    y_spline = flight_spl_roll
+    y_spline = flight_imu_p
 xmin = x.min()
 xmax = x.max()
 print "flight range = %.3f - %.3f (%.3f)" % (xmin, xmax, xmax-xmin)
@@ -147,8 +201,8 @@ qratio = 1.0
 for x in np.linspace(tmin, tmax, time*args.resample_hz):
     mqsum += abs(movie_spl_pitch(x-time_shift))
     mrsum += abs(movie_spl_yaw(x-time_shift))
-    fqsum += abs(flight_spl_pitch(x))
-    frsum += abs(flight_spl_yaw(x))
+    fqsum += abs(flight_imu_q(x))
+    frsum += abs(flight_imu_r(x))
 if fqsum > 0.001:
     qratio = mqsum / fqsum
 if mrsum > 0.001:
@@ -156,16 +210,74 @@ if mrsum > 0.001:
 print "pitch ratio:", qratio
 print "yaw ratio:", rratio
 
-# extract frames
-time = tmax - tmin
-last_time = 0.0
-for t in np.linspace(tmin, tmax, time*args.resample_hz):
-    if flight_gps_agl(t) < 5.0:
-        continue
-    if t > last_time + 2.0:
-        last_time = t
-        print t, flight_gps_lat(t), flight_gps_lon(t), flight_gps_alt(t)
-    
+if args.movie:
+    # extract frames
+    print "Opening ", args.movie
+    try:
+        capture = cv2.VideoCapture(args.movie)
+    except:
+        print "error opening video"
+
+    capture.read()
+    counter += 1
+    print "ok reading first frame"
+
+    fps = capture.get(cv2.cv.CV_CAP_PROP_FPS)
+    print "fps = %.2f" % fps
+    fourcc = int(capture.get(cv2.cv.CV_CAP_PROP_FOURCC))
+    w = capture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+    h = capture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+
+    last_time = 0.0
+    abspath = os.path.abspath(args.movie)
+    basename, ext = os.path.splitext(abspath)
+    dirname = os.path.dirname(abspath)
+    meta = dirname + "/image-metadata.txt"
+    print "writing meta data to", meta
+    f = open(meta, 'wb')
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            # no frame
+            stop_count += 1
+            print "no more frames:", stop_count
+            if stop_count > 100:
+                break
+        else:
+            stop_count = 0
+            
+        if frame == None:
+            print "Skipping bad frame ..."
+            continue
+        time = float(counter) / fps + time_shift
+        print "frame: ", counter, time
+        counter += 1
+        if time < tmin or time > tmax:
+            continue
+        if flight_gps_agl(time) < 35.0:
+            continue
+        if time > last_time + 1.0:
+            last_time = time
+            file = basename + "-%06d" % counter + ".jpg"
+            cv2.imwrite(file, frame)
+            # geotag the image
+            exif = pyexiv2.ImageMetadata(file)
+            exif.read()
+            lat_deg = float(flight_gps_lat(time))
+            lon_deg = float(flight_gps_lon(time))
+            altitude = float(flight_gps_alt(time))
+            GPS = 'Exif.GPSInfo.GPS'
+            exif[GPS + 'AltitudeRef']  = '0' if altitude >= 0 else '1'
+            exif[GPS + 'Altitude']     = Fraction(altitude)
+            exif[GPS + 'Latitude']     = decimal_to_dms(lat_deg)
+            exif[GPS + 'LatitudeRef']  = 'N' if lat_deg >= 0 else 'S'
+            exif[GPS + 'Longitude']    = decimal_to_dms(lon_deg)
+            exif[GPS + 'LongitudeRef'] = 'E' if lon_deg >= 0 else 'W'
+            exif[GPS + 'MapDatum']     = 'WGS-84'
+            exif.write()
+            head, tail = os.path.split(file)
+            f.write("%s,%.8f,%.8f,%.4f,%.4f,%.4f,%.4f\n" % (tail, flight_gps_lat(time), flight_gps_lon(time), flight_gps_alt(time), flight_imu_yaw(time)*r2d, 0.0, 0.0))
+    f.close()
 
 
 # plot the data ...
