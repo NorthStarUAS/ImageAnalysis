7a90650c6d688d1345d32d466eb561eae453a8a8
Curtis L. Olson <olson126@umn.edu>
2016-08-04 20:41:25 -0500

    Add an experimental feature to show cropped sections from all the images that a match is found within.

--- scripts/5c-mre-by-feature2.py
+++ scripts/5c-mre-by-feature2.py
@@ -18,8 +18,10 @@ import ProjectMgr
 
 parser = argparse.ArgumentParser(description='Keypoint projection.')
 parser.add_argument('--project', required=True, help='project directory')
-parser.add_argument('--stddev', type=int, default=5, help='how many stddevs above the mean for auto discarding features')
+parser.add_argument('--stddev', type=int, default=3, help='how many stddevs above the mean for auto discarding features')
 parser.add_argument('--direct', action='store_true', help='analyze direct matches (might help if initial sba fit fails.)')
+parser.add_argument('--strong', action='store_true', help='remove entire match chain, not just the worst offending element.')
+parser.add_argument('--show', action='store_true', help='show most extreme reprojection errors with matches.')
 
 args = parser.parse_args()
 
@@ -97,9 +99,9 @@ def mark_outliers(result_list, trim_stddev):
     sum = 0.0
     count = len(result_list)
 
-    # numerically it is better to sum up a list of numbers from
-    # smallest to biggest (result_list is sorted from biggest to
-    # smallest)
+    # numerically it is better to sum up a list of floatting point
+    # numbers from smallest to biggest (result_list is sorted from
+    # biggest to smallest)
     for line in reversed(result_list):
         sum += line[0]
         
@@ -121,20 +123,22 @@ def mark_outliers(result_list, trim_stddev):
         if line[0] > mre + stddev * trim_stddev:
             print "  outlier index %d-%d err=%.2f" % (line[1], line[2],
                                                       line[0])
+            if args.show:
+                draw_match(line[1], line[2])
             match = matches_direct[line[1]]
             match[line[2]+1] = [-1, -1]
             if not args.direct:
                 match = matches_sba[line[1]]
                 match[line[2]+1] = [-1, -1]
             mark_count += 1
-
+            
     # trim the result_list
     print " trimming results list..."
     for i in range(len(result_list)):
         line = result_list[i]
         if line[0] < mre + stddev * trim_stddev:
             if i > 0:
-                # delete the remainder of the sorted list
+                # remove the marked items from the sorted list
                 del result_list[0:i]
             # and break
             break
@@ -148,18 +152,78 @@ def delete_marked_matches():
         match_direct = matches_direct[i]
         if not args.direct:
             match_sba = matches_sba[i]
+        has_bad_elem = False
         for j in reversed(range(1, len(match_direct))):
             p = match_direct[j]
             if p == [-1, -1]:
+                has_bad_elem = True
                 match_direct.pop(j)
                 if not args.direct:
                     match_sba.pop(j)
-        if len(match_direct) < 3:
-            # print "deleting:", match_direct
+        if args.strong and has_bad_elem:
+            print "deleting entire match that contains a bad element"
+            matches_direct.pop(i)
+            if not args.direct:
+                matches_sba.pop(i)
+        elif len(match_direct) < 4:
+            print "deleting match that is now in less than 3 images:", match_direct
             matches_direct.pop(i)
             if not args.direct:
                 matches_sba.pop(i)
 
+# experimental, draw a visual of a match point in all it's images
+def draw_match(i, bad_index):
+    green = (0, 255, 0)
+    red = (0, 0, 255)
+
+    if not args.direct:
+        match = matches_sba[i]
+    else:
+        match = matches_direct[i]
+    print 'match:', match, 'bad index:', bad_index
+    for j, m in enumerate(match[1:]):
+        print ' ', m, proj.image_list[m[0]]
+        img = proj.image_list[m[0]]
+        kp = img.kp_list[m[1]]
+        print ' ', kp.pt
+        rgb = img.load_rgb()
+        h, w = rgb.shape[:2]
+        crop = True
+        range = 300
+        if crop:
+            cx = int(round(kp.pt[0]))
+            cy = int(round(kp.pt[1]))
+            if cx < range:
+                xshift = range - cx
+                cx = range
+            elif cx > w - range:
+                xshift = cx - (w - range)
+                cx = w - range
+            else:
+                xshift = 0
+            if cy < range:
+                yshift = range - cy
+                cy = range
+            elif cy > h - range:
+                yshift = cy - (h - range)
+                cy = h - range
+            else:
+                yshift = 0
+            print 'size:', w, h, 'shift:', xshift, yshift
+            rgb1 = rgb[cy-range:cy+range, cx-range:cx+range]
+            if ( j == bad_index ):
+                color = red
+            else:
+                color = green
+            cv2.circle(rgb1, (range-xshift,range-yshift), 2, color, thickness=2)
+        else:
+            scale = 790.0/float(w)
+            rgb1 = cv2.resize(rgb, (0,0), fx=scale, fy=scale)
+            cv2.circle(rgb1, (int(round(kp.pt[0]*scale)), int(round(kp.pt[1]*scale))), 2, green, thickness=2)
+        cv2.imshow(img.name, rgb1)
+    print 'waiting for keyboard input...'
+    key = cv2.waitKey() & 0xff
+    cv2.destroyAllWindows()
 
 result_list = compute_reprojection_errors(proj.image_list, proj.cam)
 
@@ -168,6 +232,32 @@ result_list, mark_count = mark_outliers(result_list, args.stddev)
 while mark_count > 0:
     mark_sum += mark_count
     result_list, mark_count = mark_outliers(result_list, args.stddev)
+
+# now count how many features show up in each image
+for i in proj.image_list:
+    i.feature_count = 0
+for i, match in enumerate(matches_direct):
+    for j, p in enumerate(match[1:]):
+        if p[1] != [-1, -1]:
+            image = proj.image_list[ p[0] ]
+            image.feature_count += 1
+
+# make a dict of all images with less than 25 feature matches
+weak_dict = {}
+for i, img in enumerate(proj.image_list):
+    print img.name, img.feature_count
+    if img.feature_count > 0 and img.feature_count < 25:
+        weak_dict[i] = True
+print 'weak images:', weak_dict
+
+# mark any features in the weak images list
+for i, match in enumerate(matches_direct):
+    print 'before:', match
+    for j, p in enumerate(match[1:]):
+        if p[0] in weak_dict:
+             match[j+1] = [-1, -1]
+             mark_sum += 1
+    print 'after:', match
 
 if mark_sum > 0:
     result=raw_input('Remove ' + str(mark_sum) + ' outliers from the original matches? (y/n):')
@@ -180,7 +270,6 @@ if mark_sum > 0:
         if not args.direct:
             print "Writing sba matches..."
             pickle.dump(matches_sba, open(args.project + "/matches_sba", "wb"))
-
 
 #print "Mean reprojection error = %.4f" % (mre)
 
