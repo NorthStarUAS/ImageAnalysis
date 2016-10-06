53a409ccec71cc7287d827e1d65c525eddec50b6
Curtis L. Olson <olson126@umn.edu>
2016-10-06 10:12:05 -0500

    Tweaks to best neighbor logic.

--- lib/Matcher.py
+++ lib/Matcher.py
@@ -1110,20 +1110,21 @@ def reportConnectionDetail():
 
 # return the neighbor that is closest to the root node of the
 # placement tree (i.e. smallest cycle_depth.
-def bestNeighbor(image, image_list):
+def bestNeighbor(image, image_list, match_pairs):
     best_cycle_depth = len(image_list) + 1
     best_index = None
-    for i, pairs in enumerate(image.match_list):
+    for i, pairs in enumerate(match_pairs):
         if len(pairs):
             i2 = image_list[i]
             dist = i2.cycle_depth
-            #print "  neighbor check %d = %d" % ( i, dist )
+            # print "  neighbor check %d = %d" % ( i, dist )
             if dist >= 0 and dist < best_cycle_depth:
+                # print '    new best:', dist
                 best_cycle_depth = dist
                 best_index = i
     return best_index, best_cycle_depth
 
-def groupByConnections(image_list, matches_direct):
+def groupByConnections(image_list, matches_direct, match_pairs):
     # reset the cycle distance for all images
     for image in image_list:
         image.cycle_depth = -1
@@ -1150,13 +1151,14 @@ def groupByConnections(image_list, matches_direct):
         best_cycle_depth = len(image_list) + 1
         for i, image in enumerate(image_list):
             if image.cycle_depth < 0:
-                index, cycle_depth = bestNeighbor(image, image_list)
+                index, cycle_depth = bestNeighbor(image, image_list,
+                                                  match_pairs[i])
                 if cycle_depth >= 0 and (cycle_depth+1 < best_cycle_depth):
                     best_index = i
                     best_cycle_depth = cycle_depth+1
                     done = False
         if best_index == None:
-            #print "Cannot find an unplaced image with a connected neighbor"
+            print "Cannot find an unplaced image with a connected neighbor"
             if len(group):
                 # commit the previous group (if it exists)
                 group_list.append(group)
@@ -1174,12 +1176,12 @@ def groupByConnections(image_list, matches_direct):
                         max_connections = image.connections
                         best_index = i
                         done = False
-                        #print " found image %d connections = %d" % (i, max_connections)
+                        print " found image %d connections = %d" % (i, max_connections)
         if best_index != None:
             image = image_list[best_index]
             image.cycle_depth = best_cycle_depth
             last_cycle_depth = best_cycle_depth
-            #print "Adding %s (cycles = %d)" % (image.name, best_cycle_depth)
+            print "Adding %s (cycles = %d)" % (image.name, best_cycle_depth)
             group.append(image)
 
     print "Group (cycles) report:"
