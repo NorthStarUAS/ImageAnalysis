e66e1dc229c86e6bff0a273d5e630f15b8f0fed4
Curtis L. Olson <curtolson@gmail.com>
2017-12-29 17:05:45 -0600

    Review the fully grouped match set by default.

--- scripts/4e-review-matches.py
+++ scripts/4e-review-matches.py
@@ -39,12 +39,12 @@ proj.undistort_keypoints()
 matcher = Matcher.Matcher()
 
 print("Loading match points (direct)...")
-matches_direct = pickle.load( open( os.path.join(args.project, "matches_direct"), "rb" ) )
+matches = pickle.load( open( os.path.join(args.project, "matches_grouped"), "rb" ) )
 
 # load the group connections within the image set
 groups = Groups.load(args.project)
 
-# traverse the matches_direct structure and create a pair-wise match
+# traverse the matches structure and create a pair-wise match
 # structure.  (Start with an empty n x n list of empty pair lists,
 # then fill in the structures.)
 pairs = []
@@ -60,7 +60,7 @@ for i in range(len(proj.image_list)):
     stddevs.append( [0] * len(proj.image_list) )
     status_flags.append( [None] * len(proj.image_list) )
     
-for match in matches_direct:
+for match in matches:
     for p1 in match[1:]:
         for p2 in match[2:]:
             if p1[0] >= p2[0]:
@@ -89,44 +89,48 @@ for match in matches_direct:
 bypair = []
 for i in range(len(proj.image_list)):
     for j in range(len(proj.image_list)):
-        if len(pairs[i][j]):
-            i1 = proj.image_list[i]
-            i2 = proj.image_list[j]
-            src = []
-            dst = []
-            for pair in pairs[i][j]:
-                # undistorted uv points
-                src.append( i1.uv_list[pair[0]] )
-                dst.append( i2.uv_list[pair[1]] )
-            src = np.float32(src)
-            dst = np.float32(dst)
-            method = 0          # a regular method using all the points
-            #method = cv2.RANSAC
-            #method = cv2.LMEDS
-            M, status = cv2.findHomography(src, dst, method)
-            homography[i][j] = M
-            error = []
-            for k, p in enumerate(src):
-                # *wrong* for homography: p_est = M.dot( np.hstack((p, 1.0)) )[:2]
-                tmp = M[2][0]*p[0] + M[2][1]*p[1] + M[2][2]
-                if abs(tmp) > 0.000001:
-                    x = (M[0][0]*p[0] + M[0][1]*p[1] + M[0][2]) / tmp
-                    y = (M[1][0]*p[0] + M[1][1]*p[1] + M[1][2]) / tmp
-                    p_est = np.array([x, y])
-                else:
-                    p_est = np.array([0.0, 0.0])
-                #print 'p est:', p_est, 'act:', dst[k]
-                d = np.linalg.norm(p_est - dst[k])
-                #print 'dist:', d
-                error.append(d)
-            error = np.array(error)
-            avg = np.mean(error)
-            std = np.std(error)
-            averages[i][j] = avg
-            stddevs[i][j] = std
-            status_flags[i][j] = status
-            print('pair:', i, j, 'avg:', avg, 'std:', std)
-            bypair.append( [avg, std, i, j] )
+        if not len(pairs[i][j]):
+            continue
+        i1 = proj.image_list[i]
+        i2 = proj.image_list[j]
+        src = []
+        dst = []
+        for pair in pairs[i][j]:
+            # undistorted uv points
+            src.append( i1.uv_list[pair[0]] )
+            dst.append( i2.uv_list[pair[1]] )
+        src = np.float32(src)
+        dst = np.float32(dst)
+        method = 0          # a regular method using all the points
+        #method = cv2.RANSAC
+        #method = cv2.LMEDS
+        M, status = cv2.findHomography(src, dst, method)
+        if M is None:
+            print("Homography failed, pair:", i, j, "num pairs:", len(pairs[i][j]), pairs[i][j])
+            continue
+        homography[i][j] = M
+        error = []
+        for k, p in enumerate(src):
+            # *wrong* for homography: p_est = M.dot( np.hstack((p, 1.0)) )[:2]
+            tmp = M[2][0]*p[0] + M[2][1]*p[1] + M[2][2]
+            if abs(tmp) > 0.000001:
+                x = (M[0][0]*p[0] + M[0][1]*p[1] + M[0][2]) / tmp
+                y = (M[1][0]*p[0] + M[1][1]*p[1] + M[1][2]) / tmp
+                p_est = np.array([x, y])
+            else:
+                p_est = np.array([0.0, 0.0])
+            #print 'p est:', p_est, 'act:', dst[k]
+            d = np.linalg.norm(p_est - dst[k])
+            #print 'dist:', d
+            error.append(d)
+        error = np.array(error)
+        avg = np.mean(error)
+        std = np.std(error)
+        averages[i][j] = avg
+        stddevs[i][j] = std
+        status_flags[i][j] = status
+        print('pair:', i, j, 'avg:', avg, 'std:', std)
+        bypair.append( [avg, std, i, j] )
 
 bypair = sorted(bypair, key=lambda fields: fields[0], reverse=True)
 mark_list = []
@@ -147,7 +151,7 @@ for line in bypair:
                 p0 = pairs[i][j][k][0]
                 p1 = pairs[i][j][k][1]
                 print('outlier:', i, j, p0, p1)
-                for k, match in enumerate(matches_direct):
+                for k, match in enumerate(matches):
                     pos1 = pos2 = -1
                     for pos, m in enumerate(match[1:]):
                         if m[0] == i and m[1] == p0:
@@ -165,19 +169,19 @@ if len(mark_list):
     result=input('Save these changes? (y/n):')
     if result == 'y' or result == 'Y':
         # mark and delete the outliers
-        cull.mark_using_list(mark_list, matches_direct)
-        cull.delete_marked_matches(matches_direct)
+        cull.mark_using_list(mark_list, matches)
+        cull.delete_marked_matches(matches)
  
         # write out the updated match dictionaries
         print("Writing direct matches...")
-        pickle.dump(matches_direct, open(os.path.join(args.project, "matches_direct"), "wb"))
+        pickle.dump(matches, open(os.path.join(args.project, "matches_grouped"), "wb"))
 
 print('Hard coded exit in mid script...')
 quit()
 
 # run through the match list and compute the homography error (in image space)
 error_list = []
-for k, match in enumerate(matches_direct):
+for k, match in enumerate(matches):
     max_dist = 0.0
     for p1 in match[1:]:
         for p2 in match[1:]:
@@ -210,23 +214,23 @@ error_list = sorted(error_list, key=lambda fields: fields[0], reverse=True)
 
 #for line in result:
 #    print 'index:', line[1], 'error:', line[0]
-#    cull.draw_match(line[1], 0, matches_direct, proj.image_list)
+#    cull.draw_match(line[1], 0, matches, proj.image_list)
     
-mark_list = cull.show_outliers(error_list, matches_direct, proj.image_list)
+mark_list = cull.show_outliers(error_list, matches, proj.image_list)
 mark_sum = len(mark_list)
 
 # mark both direct and/or sba match lists as requested
-cull.mark_using_list(mark_list, matches_direct)
+cull.mark_using_list(mark_list, matches)
 
 if mark_sum > 0:
     print('Outliers removed from match lists:', mark_sum)
     result=input('Save these changes? (y/n):')
     if result == 'y' or result == 'Y':
-        cull.delete_marked_matches(matches_direct)
-        print(len(matches_direct))
+        cull.delete_marked_matches(matches)
+        print(len(matches))
         
         # write out the updated match dictionaries
         print("Writing direct matches...")
-        pickle.dump(matches_direct, open(os.path.join(args.project, "matches_direct"), "wb"))
+        pickle.dump(matches, open(os.path.join(args.project, "matches_grouped"), "wb"))
 
 
