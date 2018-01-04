df87826fc6453cac8a5fb8ce3b20fcf6d78d1075
Curtis L. Olson <curtolson@gmail.com>
2018-01-04 15:00:30 -0600

    Retool the optimizer to leverage the cv2 projectPoints() function which takes K and distCoeffs as inputs.  This enables me to add these parameters to the optimizer and refine the camera calibration and distortion parameters further.

--- lib/Optimizer.py
+++ lib/Optimizer.py
@@ -3,8 +3,14 @@
 # Prep the point and camera data and run it through the scipy least
 # squares optimizer.
 #
-# This version does not try to solve for any camera parameters, just
-# the camera pose and 3d feature locations.
+# (old: This version does not try to solve for any camera parameters, just
+# the camera pose and 3d feature locations.)
+#
+# This optimizer explores using cv2 native functions to do per-image
+# reprojection, and then extract out the errors from that.  This will
+# require some 'assembly' and 'accounting' work.  It will also
+# hopefully let me add the K and distortion parameters to the
+# optimizer.
 
 import cv2
 import numpy as np
@@ -13,52 +19,48 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-import sys
-sys.path.append('../lib')
-import transformations
-
-def project(points, cam_M):
-    """Convert 3-D points to 2-D by projecting onto images."""
-    #print('points: {}'.format(points.shape))
-    #print('ones: {}'.format( np.ones((points.shape[0], 1)) ))
-    nedh = np.hstack((points, np.ones((points.shape[0], 1))))
-    #print('nedh: {}'.format(nedh))
-    #uvh = np.einsum("...ij,...i", cam_M, nedh)
-    #uvh = cam_M.dot( nedh )
-    uvh = np.zeros( (points.shape[0], 3) )
-    for i in range(points.shape[0]):
-        uvh[i] = cam_M[i].dot(nedh[i])
-    #print('uvh: {}'.format(uvh[:,2:3].shape))
-    uvh = uvh / uvh[:,2:3]
-    #print('uvh: {}'.format(uvh))
-    uv = uvh[:, 0:2]
-    #print('uv: {}'.format(uv))
-    return uv
 
 graph = None
 last_mre = 1.0e+10              # a big number
-def fun(params, n_cameras, n_points, camera_indices, point_indices, points_2d, K):
+def fun(params, n_cameras, n_points, by_camera_point_indices, by_camera_points_2d):
     """Compute residuals.
     
     `params` contains camera parameters, 3-D coordinates, and camera calibration parameters.
     """
     global last_mre
-    
+
+    error = None
     camera_params = params[:n_cameras * 6].reshape((n_cameras, 6))
-    cam_M = np.zeros((camera_params.shape[0], 3, 4))
-    for i, cam in enumerate(camera_params):
-        R, jac = cv2.Rodrigues(cam[:3])
-        PROJ = np.concatenate((R, cam[3:6].reshape(3,1)), axis=1)
-        M = K.dot( PROJ )
-        cam_M[i] = M
-    # print('cam_M: {}'.format(cam_M))
     points_3d = params[n_cameras * 6:n_cameras * 6 + n_points * 3].reshape((n_points, 3))
-    #print("calib:")
-    tmp = cam_M[camera_indices]
-    #print('tmp.shape {}'.format(tmp.shape))
-    points_proj = project(points_3d[point_indices], cam_M[camera_indices])
-    # mre
-    error = (points_proj - points_2d).ravel()
+    calib_params = params[n_cameras * 6 + n_points * 3:]
+    
+    K = np.identity(3)
+    K[0,0] = calib_params[0]
+    K[1,1] = calib_params[1]
+    K[0,2] = calib_params[2]
+    K[1,2] = calib_params[3]
+    distCoeffs = calib_params[4:]
+    #print('K:', K, 'distCoeffs', distCoeffs)
+
+    sum = 0
+    for i, cam in enumerate(camera_params):
+        rvec = cam[:3]
+        tvec = cam[3:6]
+        proj_points, jac = cv2.projectPoints(points_3d[by_camera_point_indices[i]], rvec, tvec, K, distCoeffs)
+        # print(i, points_3d[by_camera_point_indices[i]].shape, proj_points.shape, proj_points.ravel().shape)
+        sum += len(proj_points.ravel())
+        #print('cam:', proj_points)
+        #print('point2d:', by_camera_points_2d[i])
+        #print('error:', (by_camera_points_2d[i] - proj_points).ravel())
+        #print('debug:', by_camera_points_2d[i].shape, proj_points.shape)
+        if error is None:
+            #print("first time")
+            error = (by_camera_points_2d[i] - proj_points).ravel()
+            #print('error:', error)
+        else:
+            error = np.append(error, (by_camera_points_2d[i] - proj_points).ravel())
+        #print('sum:', sum, 'len error:', len(error), error.shape)
+            
     mre = np.mean(np.abs(error))
     if 1.0 - mre/last_mre > 0.001:
         # mre has improved by more than 0.1%
@@ -76,13 +78,14 @@ def fun(params, n_cameras, n_points, camera_indices, point_indices, points_2d, K
             plt.draw()
             plt.savefig('opt-plot.png', dpi=80)
             plt.pause(0.01)
-    return (points_proj - points_2d).ravel()
+    return error
 
 from scipy.sparse import lil_matrix
 
 def bundle_adjustment_sparsity(n_cameras, n_points, camera_indices, point_indices):
     m = camera_indices.size * 2
     n = n_cameras * 6 + n_points * 3
+    n += 9                      # four K params + five dist params
     A = lil_matrix((m, n), dtype=int)
     print('sparcity matrix is {} x {}'.format(m, n))
 
@@ -95,9 +98,9 @@ def bundle_adjustment_sparsity(n_cameras, n_points, camera_indices, point_indice
         A[2 * i, n_cameras * 6 + point_indices * 3 + s] = 1
         A[2 * i + 1, n_cameras * 6 + point_indices * 3 + s] = 1
 
-    #for s in range(3):
-    #    A[2 * i, n_cameras * 6 + n_points * 3 + s] = 1
-    #    A[2 * i + 1, n_cameras * 6 + n_points * 3 + s] = 1
+    for s in range(9):
+        A[2 * i, n_cameras * 6 + n_points * 3 + s] = 1
+        A[2 * i + 1, n_cameras * 6 + n_points * 3 + s] = 1
 
     print('A non-zero elements = {}'.format(A.nnz))
     
@@ -117,7 +120,8 @@ class Optimizer():
     # write the camera (motion) parameters, feature (structure)
     # parameters, and calibration (K) to files in the project
     # directory.
-    def run(self, image_list, placed_images, matches_list, K, use_sba=False):
+    def run(self, image_list, placed_images, matches_list, K, distCoeff,
+            use_sba=False):
         global graph
         
         if placed_images == None:
@@ -160,12 +164,22 @@ class Optimizer():
                 for p in match[1:]:
                     if p[0] in placed_images:
                         local_index = self.camera_map_rev[p[0]]
-                        # kp = image_list[p[0]].kp_list[p[1]].pt # distorted
-                        kp = image_list[p[0]].uv_list[p[1]]      # undistorted
+                        kp = image_list[p[0]].kp_list[p[1]].pt # orig/distorted
+                        # kp = image_list[p[0]].uv_list[p[1]]      # undistorted
                         s = "%d %.2f %.2f " % (local_index, kp[0], kp[1])
                         f.write(s)
                 f.write('\n')
         f.close()
+
+        # assemble the initial camera estimates
+        n_cameras = len(placed_images)
+        camera_params = np.empty(n_cameras * 6)
+        cam_idx = 0
+        for index in placed_images:
+            image = image_list[index]
+            rvec, tvec = image.get_proj()
+            camera_params[cam_idx*6:cam_idx*6+6] = np.append(rvec, tvec)
+            cam_idx += 1
 
         # count number of 3d points and observations
         n_points = 0
@@ -200,40 +214,48 @@ class Optimizer():
                 point_idx += 3
                 
         # assemble observations (image index, feature index, u, v)
-        camera_indices = np.empty(n_observations, dtype=int)
-        point_indices = np.empty(n_observations, dtype=int)
-        points_2d = np.empty((n_observations, 2))
-        obs_idx = 0
+        by_camera_point_indices = [ [] for i in range(n_cameras) ]
+        by_camera_points_2d = [ [] for i in range(n_cameras) ]
+        #print('by_camera:', by_camera)
+        #points_2d = np.empty((n_observations, 2))
+        #obs_idx = 0
         for i, match in enumerate(matches_list):
-            used = False
             for p in match[1:]:
                 if p[0] in placed_images:
                     cam_index = self.camera_map_rev[p[0]]
                     feat_index = self.feat_map_fwd[i]
                     uv = image_list[p[0]].uv_list[p[1]]
-                    camera_indices[obs_idx] = cam_index
-                    point_indices[obs_idx] = feat_index
-                    points_2d[obs_idx] = uv
-                    obs_idx += 1
+                    by_camera_point_indices[cam_index].append(feat_index)
+                    by_camera_points_2d[cam_index].append(uv)
+                    #camera_indices[obs_idx] = cam_index
+                    #point_indices[obs_idx] = feat_index
+                    #obs_idx += 1
+        # convert to numpy native structures
+        for i in range(n_cameras):
+            size = len(by_camera_point_indices[i])
+            by_camera_point_indices[i] = np.array(by_camera_point_indices[i])
+            by_camera_points_2d[i] = np.asarray([by_camera_points_2d[i]]).reshape(size, 1, 2)
 
-        # assemble the initial camera estimates
-        n_cameras = len(placed_images)
-        camera_params = np.empty(n_cameras * 6)
-        cam_idx = 0
-        for index in placed_images:
-            image = image_list[index]
-            rvec, tvec = image.get_proj()
-            camera_params[cam_idx*6:cam_idx*6+6] = np.append(rvec, tvec)
-            cam_idx += 1
-
-        #return camera_params, points_3d, camera_indices, point_indices, points_2d
-
+        # generate the camera and point indices (for mapping the
+        # sparse jacobian entries which define which observations
+        # depend on which parameters.)
+        camera_indices = np.empty(n_observations, dtype=int)
+        point_indices = np.empty(n_observations, dtype=int)
+        obs_idx = 0
+        for i in range(n_cameras):
+            for j in range(len(by_camera_point_indices[i])):
+                camera_indices[obs_idx] = i
+                point_indices[obs_idx] = by_camera_point_indices[i][j]
+                obs_idx += 1
+        print("num observations:", obs_idx)
+            
         # def run(self, mode=''):
-        x0 = np.hstack((camera_params.ravel(), points_3d.ravel()))
+        x0 = np.hstack((camera_params.ravel(), points_3d.ravel(),
+                        K[0,0], K[1,1], K[0,2], K[1,2], distCoeff))
         print('x0:')
         print(x0.shape)
         print(x0)
-        f0 = fun(x0, n_cameras, n_points, camera_indices, point_indices, points_2d, K)
+        f0 = fun(x0, n_cameras, n_points, by_camera_point_indices, by_camera_points_2d)
         #plt.figure()
         #plt.plot(f0)
         mre_start = np.mean(np.abs(f0))
@@ -276,18 +298,23 @@ class Optimizer():
         plt.pause(0.01)
         
         t0 = time.time()
-#        res = least_squares(fun, x0, bounds=[lower, upper], jac_sparsity=A,
-#                            verbose=2,
-#                            x_scale='jac',
-#                            ftol=1e-3, method='trf',
-#                            args=(n_cameras, n_points, camera_indices,
-#                                  point_indices, points_2d, K))
-        res = least_squares(fun, x0, jac_sparsity=A,
-                            verbose=2,
-                            x_scale='jac',
-                            ftol=1e-3, method='trf',
-                            args=(n_cameras, n_points, camera_indices,
-                                  point_indices, points_2d, K))
+        with_bounds = False
+        if with_bounds:
+            res = least_squares(fun, x0, bounds=[lower, upper], jac_sparsity=A,
+                                verbose=2,
+                                x_scale='jac',
+                                ftol=1e-3, method='trf',
+                                args=(n_cameras, n_points,
+                                      by_camera_point_indices,
+                                      by_camera_points_2d))
+        else:
+            res = least_squares(fun, x0, jac_sparsity=A,
+                                verbose=2,
+                                x_scale='jac',
+                                ftol=1e-3, method='trf',
+                                args=(n_cameras, n_points,
+                                      by_camera_point_indices,
+                                      by_camera_points_2d))
         t1 = time.time()
         print("Optimization took {0:.0f} seconds".format(t1 - t0))
         # print(res['x'])
