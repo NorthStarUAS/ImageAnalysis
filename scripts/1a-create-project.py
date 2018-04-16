5730782ce6eb487e21171f6aa83a6a90361be703
Curtis L. Olson <curtolson@flightgear.org>
2018-04-16 18:11:28 -0500

    Specify the image source directory when creating/initing a project.

--- scripts/1a-create-project.py
+++ scripts/1a-create-project.py
@@ -8,9 +8,11 @@ import ProjectMgr
 # create an empty project
 
 parser = argparse.ArgumentParser(description='Create an empty project.')
-parser.add_argument('--project', required=True, help='project directory')
+parser.add_argument('--project', required=True, help='project work directory')
+parser.add_argument('--images', required=True, help='image source directory')
 
 args = parser.parse_args()
 
 proj = ProjectMgr.ProjectMgr(args.project, create=True)
+proj.set_images_source(args.images)
 proj.save()
