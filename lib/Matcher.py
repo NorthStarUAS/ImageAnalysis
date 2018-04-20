cb1e9f4298d67694b0be69fb497784be779a06a7
Curtis L. Olson <curtolson@flightgear.org>
2018-04-20 13:06:54 -0500

    Fix a dumb oops ... was always clipping to best 100 matches no matter how many I actually asked for.  Now fixed.

--- lib/Matcher.py
+++ lib/Matcher.py
@@ -309,10 +309,10 @@ class Matcher():
                     matches_thresh.append(line[1])
             print('  quality matches:', len(matches_thresh))
             # fixme, make this a command line option or parameter?
-            mymax = 750
+            mymax = 500
             if len(matches_thresh) > mymax:
                 # clip list to n best rated matches
-                matches_thresh = matches_thresh[:100]
+                matches_thresh = matches_thresh[:mymax]
                 print('  clipping to:', mymax)
             
         if len(matches_thresh) < self.min_pairs:
