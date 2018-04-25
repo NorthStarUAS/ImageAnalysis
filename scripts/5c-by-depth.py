e831525a4d29c12d051336d286e81c19872bb40c
Curtis L. Olson <curtolson@flightgear.org>
2018-04-25 15:30:34 -0500

    Identify and filter features that are placed at a significant distance from the camera (depth) relative to the other features in that image. (revive this script.)

--- scripts/5c-by-depth.py
+++ scripts/5c-by-depth.py
@@ -17,15 +17,14 @@
 # shots tend to show up at the fringes of the data set due to turns
 # and often have poor connectivity and aren't as useful anyway.
 
-import sys
-sys.path.insert(0, "/usr/local/lib/python2.7/site-packages/")
-
 import argparse
 import pickle
 import cv2
 #import json
 import math
 import numpy as np
+import os
+import sys
 
 sys.path.append('../lib')
 import Groups
@@ -35,21 +34,27 @@ import match_culling as cull
 
 parser = argparse.ArgumentParser(description='Keypoint projection.')
 parser.add_argument('--project', required=True, help='project directory')
-parser.add_argument('--std', type=float, default=3, help='how many standard deviations above the mean for auto discarding features')
+parser.add_argument('--stddev', type=float, default=3, help='how many standard deviations above the mean for auto discarding features')
 parser.add_argument('--interactive', action='store_true', help='interactively review reprojection errors from worst to best and select for deletion or keep.')
 
 args = parser.parse_args()
 
 proj = ProjectMgr.ProjectMgr(args.project)
-proj.load_image_info()
+proj.load_images_info()
 proj.load_features()
 proj.undistort_keypoints()
 
-print("Loading original (direct) matches ...")
-matches_direct = pickle.load( open( args.project + "/matches_direct", "rb" ) )
+source = 'matches_grouped'
+print("Loading matches:", source)
+matches_orig = pickle.load( open( os.path.join(args.project, source), "rb" ) )
+print('Number of original features:', len(matches_orig))
+print("Loading optimized matches: matches_opt")
+matches_opt = pickle.load( open( os.path.join(args.project, "matches_opt"), "rb" ) )
+print('Number of optimized features:', len(matches_opt))
 
-print("Loading fitted (sba) matches...")
-matches_sba = pickle.load( open( args.project + "/matches_sba", "rb" ) )
+# load the group connections within the image set
+groups = Groups.load(args.project)
+print('Main group size:', len(groups[0]))
 
 # compute the depth of each feature for each image
 def compute_feature_depths(image_list, group, matches):
@@ -65,7 +70,7 @@ def compute_feature_depths(image_list, group, matches):
         for p in match[1:]:
             if p[0] in group:
                 image = image_list[p[0]]
-                cam_ned, ypr, quat = image.get_camera_pose_sba()
+                cam_ned, ypr, quat = image.get_camera_pose(opt=True)
                 dist = np.linalg.norm(feat_ned - cam_ned)
                 image.z_list.append(dist)
 
@@ -86,15 +91,17 @@ def compute_feature_depths(image_list, group, matches):
     error_list = []
     for i, match in enumerate(matches):
         feat_ned = match[0]
-        error_sum = 0
+        metric_sum = 0
         for p in match[1:]:
             if p[0] in group:
                 image = image_list[p[0]]
-                cam_ned, ypr, quat = image.get_camera_pose_sba()
+                cam_ned, ypr, quat = image.get_camera_pose(opt=True)
                 dist = np.linalg.norm(feat_ned - cam_ned)
-                error_sum += abs(dist - image.z_avg)
-        error = error_sum / len(match[1:])
-        error_list.append( [error, i, 0] )
+                dist_error = abs(dist - image.z_avg)
+                dist_metric = dist_error / image.z_std
+                metric_sum += dist_metric
+        metric_avg = metric_sum / len(match[1:])
+        error_list.append( [metric_avg, i, 0] )
 
     # sort by error, worst is first
     error_list = sorted(error_list, key=lambda fields: fields[0],
@@ -114,48 +121,45 @@ def mark_outliers(error_list, trim_stddev):
         
     # stats on error values
     print(" computing stats...")
-    mre = sum / count
+    mean = sum / count
     stddev_sum = 0.0
     for line in error_list:
         error = line[0]
-        stddev_sum += (mre-error)*(mre-error)
+        stddev_sum += (mean-error)*(mean-error)
     stddev = math.sqrt(stddev_sum / count)
-    print("mre = %.4f stddev = %.4f" % (mre, stddev))
+    print("mean = %.4f stddev = %.4f" % (mean, stddev))
 
     # mark match items to delete
     print(" marking outliers...")
     mark_count = 0
     for line in error_list:
         # print "line:", line
-        if line[0] > mre + stddev * trim_stddev:
-            cull.mark_outlier(matches_direct, line[1], line[2], line[0])
-            cull.mark_outlier(matches_sba, line[1], line[2], line[0])
+        if line[0] > mean + stddev * trim_stddev:
+            cull.mark_outlier(matches_orig, line[1], line[2], line[0])
+            cull.mark_outlier(matches_opt, line[1], line[2], line[0])
             mark_count += 1
             
     return mark_count
 
-# load the group connections within the image set
-groups = Groups.load(args.project)
-
-error_list = compute_feature_depths(proj.image_list, groups[0], matches_sba)
+error_list = compute_feature_depths(proj.image_list, groups[0], matches_opt)
 
 if args.interactive:
     # interactively pick outliers
-    mark_list = cull.show_outliers(error_list, matches_sba, proj.image_list)
+    mark_list = cull.show_outliers(error_list, matches_opt, proj.image_list)
 
-    # mark both direct and/or sba match lists as requested
-    cull.mark_using_list(mark_list, matches_direct)
-    cull.mark_using_list(mark_list, matches_sba)
+    # mark both direct and optimized match lists as requested
+    cull.mark_using_list(mark_list, matches_orig)
+    cull.mark_using_list(mark_list, matches_opt)
     mark_sum = len(mark_list)
 else:
     # trim outliers by some # of standard deviations high
-    mark_sum = mark_outliers(error_list, args.std)
+    mark_sum = mark_outliers(error_list, args.stddev)
 
 # after marking the bad matches, now count how many remaining features
 # show up in each image
 for i in proj.image_list:
     i.feature_count = 0
-for i, match in enumerate(matches_direct):
+for i, match in enumerate(matches_orig):
     for j, p in enumerate(match[1:]):
         if p[1] != [-1, -1]:
             image = proj.image_list[ p[0] ]
@@ -170,7 +174,7 @@ for i, img in enumerate(proj.image_list):
 print('weak images:', weak_dict)
 
 # mark any features in the weak images list
-for i, match in enumerate(matches_direct):
+for i, match in enumerate(matches_orig):
     #print 'before:', match
     for j, p in enumerate(match[1:]):
         if p[0] in weak_dict:
@@ -180,15 +184,13 @@ for i, match in enumerate(matches_direct):
 
 if mark_sum > 0:
     print('Outliers removed from match lists:', mark_sum)
-    result=raw_input('Save these changes? (y/n):')
+    result = input('Save these changes? (y/n):')
     if result == 'y' or result == 'Y':
-        cull.delete_marked_matches(matches_direct)
-        cull.delete_marked_matches(matches_sba)
+        cull.delete_marked_matches(matches_orig)
+        cull.delete_marked_matches(matches_opt)
         
         # write out the updated match dictionaries
-        print("Writing direct matches...")
-        pickle.dump(matches_direct, open(args.project+"/matches_direct", "wb"))
-
-        print("Writing sba matches...")
-        pickle.dump(matches_sba, open(args.project + "/matches_sba", "wb"))
-
+        print("Writing original matches...")
+        pickle.dump(matches_orig, open(os.path.join(args.project, source), "wb"))
+        print("Writing optimized matches...")
+        pickle.dump(matches_opt, open(os.path.join(args.project, "matches_opt"), "wb"))
