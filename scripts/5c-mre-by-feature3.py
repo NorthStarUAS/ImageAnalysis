468b5f61a2668a798a24c665c9785fb7ccc253e5
Curtis L. Olson <curtolson@flightgear.org>
2018-04-27 13:00:33 -0500

    Work with direct match file now (versus grouped matches.)

--- scripts/5c-mre-by-feature3.py
+++ scripts/5c-mre-by-feature3.py
@@ -30,7 +30,7 @@ proj.load_images_info()
 proj.load_features()
 proj.undistort_keypoints(optimized=True)
 
-source = 'matches_grouped'
+source = 'matches_direct'
 print("Loading matches:", source)
 matches_orig = pickle.load( open( os.path.join(args.project, source), "rb" ) )
 print('Number of original features:', len(matches_orig))
