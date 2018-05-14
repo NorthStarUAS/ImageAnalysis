489131a120744187e907b288fd84e1651831795b
Curtis L. Olson <curtolson@flightgear.org>
2018-05-14 16:36:50 -0500

    Recreate match pairs from matches_direct file.

--- scripts/4f-show-match-pairs.py
+++ scripts/4f-show-match-pairs.py
@@ -37,7 +37,31 @@ args = parser.parse_args()
 proj = ProjectMgr.ProjectMgr(args.project)
 proj.load_images_info()
 proj.load_features()
-proj.load_match_pairs(extra_verbose=False)
+if args.direct:
+    # recreate the pair-wise match structure
+    matches_list = pickle.load( open( os.path.join(args.project, "matches_direct"), "rb" ) )
+    for i1 in proj.image_list:
+        i1.match_list = []
+        for i2 in proj.image_list:
+            i1.match_list.append([])
+    for match in matches_list:
+        for p1 in match[1:]:
+            for p2 in match[1:]:
+                if p1 == p2:
+                    pass
+                else:
+                    i = p1[0]
+                    j = p2[0]
+                    image = proj.image_list[i]
+                    image.match_list[j].append( [p1[1], p2[1]] )
+    # for i in range(len(proj.image_list)):
+    #     print(len(proj.image_list[i].match_list))
+    #     print(proj.image_list[i].match_list)
+    #     for j in range(len(proj.image_list)):
+    #         print(i, j, len(proj.image_list[i].match_list[j]),
+    #               proj.image_list[i].match_list[j])
+else:
+    proj.load_match_pairs(extra_verbose=False)
 
 # lookup ned reference
 ref_node = getNode("/config/ned_reference", True)
@@ -54,7 +78,7 @@ if args.image:
     if i1 != None:
         for j, i2 in enumerate(proj.image_list):
             if len(i1.match_list[j]):
-                print("Showing %s vs %s" % (i1.name, i2.name))
+                print("Showing %s vs %s (%d matches)" % (i1.name, i2.name, len(i1.match_list[j])))
                 status = m.showMatchOrient(i1, i2, i1.match_list[j],
                                            orient=args.orient)
     else:
@@ -69,32 +93,6 @@ elif args.index:
                                            orient=args.orient)
     else:
         print("Cannot locate:", args.index)
-elif args.direct or args.sba:
-    if args.direct:
-        matches_list = pickle.load( open( args.project + "/matches_direct", "rb" ) )
-    else:
-        matches_list = pickle.load( open( args.project + "/matches_sba", "rb" ) )
-    for i, i1 in enumerate(proj.image_list):
-        for j, i2 in enumerate(proj.image_list):
-            if j <= i:
-                continue
-            # a little inefficient, but just used for debugging ...
-            matches = []
-            for match in matches_list:
-                p1 = None
-                p2 = None
-                for p in match[1:]:
-                    if p[0] == i:
-                        p1 = p[1]
-                    if p[0] == j:
-                        p2 = p[1]
-                if p1 != None and p2 != None:
-                    matches.append( [p1, p2] )
-            if len(matches):
-                print("Showing (direct) %s (%d) vs %s (%d)" % (i1.name, i
-                                                               i2.name, j))
-                status = m.showMatchOrient(i1, i2, matches,
-                                           orient=args.orient)
 elif args.order == 'sequential':
     for i, i1 in enumerate(proj.image_list):
         for j, i2 in enumerate(proj.image_list):
