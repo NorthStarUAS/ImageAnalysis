217f9bef1e8d9a9d91b70d0225cbd5aeb17ec4e9
Curtis L. Olson <curtolson@flightgear.org>
2019-01-22 10:43:15 -0600

    Comment out some debugging code.

--- scripts/4f-image-groups.py
+++ scripts/4f-image-groups.py
@@ -76,26 +76,26 @@ pickle.dump(matches_used, open(os.path.join(area_dir, "matches_used"), "wb"))
 # this is extra (and I'll put it here for now for lack of a better
 # place), but for visualization's sake, create a gnuplot data file
 # that will show all the match connectivity in the set.
-file = os.path.join(area_dir, 'connections.gnuplot')
-f = open(file, 'w')
-pair_dict = {}
-for match in matches:
-    for m1 in match[1:]:
-        for m2 in match[1:]:
-            if m1[0] == m2[0]:
-                # skip selfies
-                continue
-            key = '%d %d' % (m1[0], m2[0])
-            pair_dict[key] = [m1[0], m2[0]]
-for pair in pair_dict:
-    #print 'pair:', pair, pair_dict[pair]
-    image1 = proj.image_list[pair_dict[pair][0]]
-    image2 = proj.image_list[pair_dict[pair][1]]
-    (ned1, ypr1, quat1) = image1.get_camera_pose()
-    (ned2, ypr2, quat2) = image2.get_camera_pose()
-    f.write("%.2f %.2f\n" % (ned1[1], ned1[0]))
-    f.write("%.2f %.2f\n" % (ned2[1], ned2[0]))
-    f.write("\n")
-f.close()       
+# file = os.path.join(area_dir, 'connections.gnuplot')
+# f = open(file, 'w')
+# pair_dict = {}
+# for match in matches:
+#     for m1 in match[1:]:
+#         for m2 in match[1:]:
+#             if m1[0] == m2[0]:
+#                 # skip selfies
+#                 continue
+#             key = '%d %d' % (m1[0], m2[0])
+#             pair_dict[key] = [m1[0], m2[0]]
+# for pair in pair_dict:
+#     #print 'pair:', pair, pair_dict[pair]
+#     image1 = proj.image_list[pair_dict[pair][0]]
+#     image2 = proj.image_list[pair_dict[pair][1]]
+#     (ned1, ypr1, quat1) = image1.get_camera_pose()
+#     (ned2, ypr2, quat2) = image2.get_camera_pose()
+#     f.write("%.2f %.2f\n" % (ned1[1], ned1[0]))
+#     f.write("%.2f %.2f\n" % (ned2[1], ned2[0]))
+#     f.write("\n")
+# f.close()       
     
     
