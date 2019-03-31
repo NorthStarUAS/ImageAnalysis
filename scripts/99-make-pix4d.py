e69d3e1e8d056d73c1aea0e1e2444b949904778a
Curtis L. Olson <curtolson@flightgear.org>
2019-03-31 10:48:31 -0500

    Updates to make-pix4d script to support overriding heading and altitude. This supports flying with field agent which forces the Phantom to always a fly at a 90 degree east orientation no matter what direction it is moving. Also redo the altitude override syntax for dji phantom 4 pro v2 images which don't have a useful geotagged altitude.

--- scripts/99-make-pix4d.py
+++ scripts/99-make-pix4d.py
@@ -10,15 +10,26 @@ import os
 import pyexiv2
 
 from auracore import wgs84      # github.com/AuraUAS/aura-core
+from props import getNode
+
+from lib import ProjectMgr
+
+d2r = math.pi / 180.0
+r2d = 180.0 / math.pi
 
 parser = argparse.ArgumentParser(description='Create a pix4d.csv file for a folder of geotagged images.')
-parser.add_argument('--images', required=True, help='Directory containing the images')
-parser.add_argument('--fudge-altitude-dji', default=0.0, type=float, help='Fudge altitude geotag for stupid dji(drone deploy?)')
+parser.add_argument('--project', required=True, help='project directory')
+parser.add_argument('--force-heading', type=float, help='Force heading for every image')
+parser.add_argument('--force-altitude', type=float, help='Fudge altitude geotag for stupid dji phantom 4 pro v2.0')
 args = parser.parse_args()
+
+proj = ProjectMgr.ProjectMgr(args.project)
 
 # load list of images
 files = []
-for file in os.listdir(args.images):
+dir_node = getNode('/config/directories', True)
+image_dir = os.path.normpath(dir_node.getStringEnum('image_sources', 0))
+for file in os.listdir(image_dir):
     if fnmatch.fnmatch(file, '*.jpg') or fnmatch.fnmatch(file, '*.JPG'):
         files.append(file)
 files.sort()
@@ -40,10 +51,8 @@ def dms_to_decimal(degrees, minutes, seconds, sign=' '):
 images = []
 # read image exif timestamp (and convert to unix seconds)
 #bar = Bar('Scanning image dir:', max = len(files))
-last_lon = None
-last_lat = None
 for file in files:
-    name = os.path.join(args.images, file)
+    name = os.path.join(image_dir, file)
     #print(name)
     exif = pyexiv2.ImageMetadata(name)
     exif.read()
@@ -60,7 +69,7 @@ for file in files:
     elon = exif[GPS + 'Longitude'].value
     lon = dms_to_decimal(elon[0], elon[1], elon[2], exif[GPS + 'LongitudeRef'].value)
     ealt = exif[GPS + 'Altitude']
-    alt = float(ealt.value) + args.fudge_altitude_dji
+    alt = float(ealt.value)
     #exif[GPS + 'MapDatum'])
 
     # print exif.exif_keys
@@ -72,31 +81,59 @@ for file in files:
         t = datetime.time(int(hour), int(minute), int(second))
         dt = datetime.datetime.combine(d, t) 
         unixtime = float(dt.strftime('%s'))
-    if last_lat and last_lon:
-        (heading, rev_course, leg_dist) = \
-            wgs84.geo_inverse( last_lat, last_lon, lat, lon )
-    else:
-        heading = None
     #print('pos:', lat, lon, alt, heading)
-    last_lat = lat
-    last_lon = lon
-    images.append([file, lat, lon, alt, 0.0, 0.0, heading])
+    line = [file, lat, lon]
+    if not args.force_altitude:
+        line.append(alt)
+    else:
+        line.append(args.force_altitude)
+    images.append(line)
     #bar.next()
 #bar.finish()
 
-# attempt to fix up the headings at the discontinuities
-if len(images) > 1:
-    images[0][6] = images[1][6]
+for i in range(len(images)):
+    if i > 0:
+        prev = images[i-1]
+    else:
+        prev = None
+    cur = images[i]
+    if i < len(images)-1:
+        next = images[i+1]
+    else:
+        next = None
 
-for i in range(1, len(images)-1):
-    if abs(images[i][6] - images[i-1][6]) > 45.0:
-        images[i][6] = images[i+1][6]    
+    if not prev is None:
+        (prev_hdg, rev_course, prev_dist) = \
+            wgs84.geo_inverse( prev[1], prev[2], cur[1], cur[2] )
+    else:
+        prev_hdg = 0.0
+        prev_dist = 0.0
+    if not next is None:
+        (next_hdg, rev_course, next_dist) = \
+            wgs84.geo_inverse( cur[1], cur[2], next[1], next[2] )
+    else:
+        next_hdg = 0.0
+        next_dist = 0.0
+        
+    prev_hdgx = math.cos(prev_hdg*d2r)
+    prev_hdgy = math.sin(prev_hdg*d2r)
+    next_hdgx = math.cos(next_hdg*d2r)
+    next_hdgy = math.sin(next_hdg*d2r)
+    avg_hdgx = (prev_hdgx*prev_dist + next_hdgx*next_dist) / (prev_dist + next_dist)
+    avg_hdgy = (prev_hdgy*prev_dist + next_hdgy*next_dist) / (prev_dist + next_dist)
+    avg_hdg = math.atan2(avg_hdgy, avg_hdgx)*r2d
+    if avg_hdg < 0:
+        avg_hdg += 360.0
+    print("%d %.2f %.1f %.2f %.1f %.2f" % (i, prev_hdg, prev_dist, next_hdg, next_dist, avg_hdg))
+    images[i].append(0)
+    images[i].append(0)
+    if args.force_heading:
+        images[i].append(args.force_heading)
+    else:
+        images[i].append(avg_hdg)
 
-#for line in images:
-#    print(line)
-    
 # traverse the image list and create output csv file
-output_file = os.path.join(args.images, 'pix4d.csv')
+output_file = os.path.join(image_dir, 'pix4d.csv')
 with open(output_file, 'w') as csvfile:
     writer = csv.DictWriter( csvfile,
                              fieldnames=['File Name',
