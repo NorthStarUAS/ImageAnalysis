35f4698fbb590c1873bec1ec890719e0a8a5286e
Curtis L. Olson <curtolson@flightgear.org>
2019-05-10 11:40:20 -0500

    Experiment for generating better fitting surface models for rendering.

--- scripts/lib/Panda3d.py
+++ scripts/lib/Panda3d.py
@@ -76,9 +76,7 @@ def generate_from_grid(proj, group, ref_image=False, src_dir=".",
         # this is contructed in a weird way, but we generate the 2d
         # iteration in the same order that the original grid_list was
         # constucted so it works.
-        width, height = image.get_size()
-        if not width or not height:
-            width, height = proj.cam.get_image_params()
+        width, height = proj.cam.get_image_params()
         steps = int(math.sqrt(len(image.grid_list))) - 1
         n = 1
         nan_list = []
@@ -123,7 +121,53 @@ def generate_from_grid(proj, group, ref_image=False, src_dir=".",
             # this image.  For now let's warn and delete the model
             print("Warning: no polygons fully on surface, removing:", name)
             os.remove(name)
-            
+
+def share_edge(label, uv1, uv2, h, w):
+    if uv1[0] == uv2[0]:
+        if uv1[0] == 0 or uv1[0] == w:
+            return True
+    elif uv1[1] == uv2[1]:
+        if uv1[1] == 0 or uv1[1] == h:
+            return True
+    else:
+        return False
+    
+# use law of cosines and edge point data to determine if a side of the
+# triangle lies on the edge and also has a skinny edge angle.
+def is_skinny(tri, pts, uvs, h, w, thresh=0.2):
+    A = np.array(pts[tri[0]])
+    B = np.array(pts[tri[1]])
+    C = np.array(pts[tri[2]])
+    uvA = uvs[tri[0]]
+    uvB = uvs[tri[1]]
+    uvC = uvs[tri[2]]
+    va = A - C
+    vb = C - B
+    vc = B - A
+    a2 = va[0]*va[0] + va[1]*va[1]
+    b2 = vb[0]*vb[0] + vb[1]*vb[1]
+    c2 = vc[0]*vc[0] + vc[1]*vc[1]
+    a = math.sqrt(a2)
+    b = math.sqrt(b2)
+    c = math.sqrt(c2)
+    print("pts:", A, B, C)
+    print("sides:", a, b, c)
+    tmp1 = np.clip((a2 + c2 - b2)/(2*a*c), -1.0, 1.0)
+    tmp2 = np.clip((a2 + b2 - c2)/(2*a*b), -1.0, 1.0)
+    alpha = math.acos(tmp1)
+    beta = math.acos(tmp2)
+    gamma = math.pi - (alpha + beta)
+    print("angles:", alpha, beta, gamma)
+    skinny = False
+    if share_edge("c", uvA, uvB, h, w) and (alpha < thresh or gamma < thresh):
+        skinny = True
+    elif share_edge("b", uvB, uvC, h, w) and (gamma < thresh or beta < thresh):
+        skinny = True
+    elif share_edge("a", uvC, uvA, h, w) and (beta < thresh or alpha < thresh):
+        skinny = True
+    if skinny: print("skinny")
+    return skinny
+    
 def generate_from_fit(proj, group, ref_image=False, src_dir=".",
                       analysis_dir=".", resolution=512 ):
     # make the textures if needed
@@ -131,6 +175,7 @@ def generate_from_fit(proj, group, ref_image=False, src_dir=".",
 
     for name in group:
         image = proj.findImageByName(name)
+        print("generate from fit:", image)
         if len(image.fit_xy) < 3:
             continue
             print("Warning: removing egg file, no polygons for:", name)
@@ -146,9 +191,7 @@ def generate_from_fit(proj, group, ref_image=False, src_dir=".",
         f.write("<Texture> tex { \"" + image.name + ".JPG\" }\n\n")
         f.write("<VertexPool> surface {\n")
 
-        width, height = image.get_size()
-        if not width or not height:
-            width, height = proj.cam.get_image_params()
+        width, height = proj.cam.get_image_params()
         n = 1
         #print("uv len:", len(image.fit_uv))
         for i in range(len(image.fit_xy)):
@@ -156,7 +199,9 @@ def generate_from_fit(proj, group, ref_image=False, src_dir=".",
             f.write("    %.2f %.2f %.2f\n" % (image.fit_xy[i][0],
                                               image.fit_xy[i][1],
                                               image.fit_z[i]))
-            f.write("    <UV> { %.5f %.5f }\n" % (image.fit_uv[i][0]/float(width), 1.0-image.fit_uv[i][1]/float(height)))
+            u = np.clip(image.fit_uv[i][0]/float(width), 0, 1.0)
+            v = np.clip(1.0-image.fit_uv[i][1]/float(height), 0, 1.0)
+            f.write("    <UV> { %.5f %.5f }\n" % (u, v))
             f.write("  }\n")
             n += 1
         f.write("}\n\n")
@@ -165,11 +210,17 @@ def generate_from_fit(proj, group, ref_image=False, src_dir=".",
 
         tris = scipy.spatial.Delaunay(np.array(image.fit_xy))
         for tri in tris.simplices:
-            f.write("  <Polygon> {\n")
-            f.write("   <TRef> { tex }\n")
-            f.write("   <Normal> { 0 0 1 }\n")
-            f.write("   <VertexRef> { %d %d %d <Ref> { surface } }\n" \
-                    % (tri[0]+1, tri[1]+1, tri[2]+1))
-            f.write("  }\n")
+            edge_count = image.fit_edge[tri[0]] \
+                + image.fit_edge[tri[1]] \
+                + image.fit_edge[tri[2]]
+            if is_skinny(tri, image.fit_xy, image.fit_uv, height, width):
+                print("skinny edge skipping")
+            else:
+                f.write("  <Polygon> {\n")
+                f.write("   <TRef> { tex }\n")
+                f.write("   <Normal> { 0 0 1 }\n")
+                f.write("   <VertexRef> { %d %d %d <Ref> { surface } }\n" \
+                        % (tri[0]+1, tri[1]+1, tri[2]+1))
+                f.write("  }\n")
         f.write("}\n")
         f.close()
