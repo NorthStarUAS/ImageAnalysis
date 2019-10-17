aa1eea08f0da7fbf5e433c462ab3c27d2e5ce146
Curtis L. Olson <olson126@umn.edu>
2019-10-17 15:54:11 -0500

    Add color info to the classifier (optional)

--- scripts/sandbox/lbp.py
+++ scripts/sandbox/lbp.py
@@ -8,6 +8,8 @@ from sklearn.svm import LinearSVC  # pip3 install scikit-learn
 import numpy as np
 import matplotlib.pyplot as plt
 
+texture_and_color = False
+
 def describe(gray, eps=1e-7):
     radius = 4
     numPoints = 8 * radius
@@ -45,17 +47,29 @@ args = parser.parse_args()
 
 rgb = cv2.imread(args.image, flags=cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH|cv2.IMREAD_IGNORE_ORIENTATION)
 (h, w) = rgb.shape[:2]
-if True:
+gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+if False:
+    hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
+    hue, sat, val = cv2.split(hsv)
+    # cv2 hue range: 0 - 179
+    target_hue_value = 0          # red = 0
+    t1 = np.mod((hue.astype('float') + 90), 180) - 90
+    print('t1:', np.min(t1), np.max(t1))
+    cv2.imshow('t1', cv2.resize(t1, (int(w*args.scale), int(h*args.scale))))
+    dist = np.abs(target_hue_value - t1)
+    print('dist:', np.min(dist), np.max(dist))
+    gray = (255 - dist * 256 / 90).astype('uint8')
+elif True:
     g, b, r = cv2.split(rgb)
     g[g==0] = 1
     r[r==0] = 1
-    #ng = normalize(g)
-    #nr = normalize(r)
-    num = r.astype('float') - g.astype('float')
-    den = r.astype('float') + g.astype('float')
-    gray = (255 * normalize( num / den) ).astype('uint8')
-else:
-    gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+    ng = g.astype('float') / 255.0
+    nr = r.astype('float') / 255.0
+    index = (nr - ng) / (nr + ng)
+    print("range:", np.min(index), np.max(index))
+    #index[index<0.25] = -1.0
+    index = ((0.5 * index + 0.5) * 255).astype('uint8')
+    cv2.imshow('index', cv2.resize(index, (int(w*args.scale), int(h*args.scale))))
 
 radius = 3                      # this is a tuning dial
 numPoints = 8 * radius
@@ -74,20 +88,31 @@ def draw(image, col, row, step, color, width):
                   (int((col+step)*args.scale)-1, int((row+step)*args.scale)-1),
                   color=color, thickness=width)
 
+def gen_classifier(lbp, index, row, col, step):
+    lbp_region = lbp[row:row+step,col:col+step]
+    (hist, _) = np.histogram(lbp_region.ravel(),
+                             bins=np.arange(0, numPoints + 3),
+                             range=(0, numPoints + 2))
+    if texture_and_color:
+        index_region = index[row:row+step,col:col+step]
+        (index_hist, _) = np.histogram(index_region.ravel(),
+                                       bins=64,
+                                       range=(0, 255))
+        #index_hist[0] = 0
+        hist = np.concatenate((hist, index_hist), axis=None)
+    return hist
+ 
 def update_guess(image, step, scale, model):
     row = 0
     (h, w) = image.shape[:2]
     while row + step < h:
         col = 0
         while col + step < w:
-            lbp_region = lbp[row:row+step,col:col+step]
-            (hist, _) = np.histogram(lbp_region.ravel(),
-                                     bins=np.arange(0, numPoints + 3),
-                                     range=(0, numPoints + 2))
+            hist = gen_classifier(lbp, index, row, col, step)
             prediction = model.predict(hist.reshape(1, -1))
             if prediction == 'no':
                 draw(scale, col, row, step, (0,255,0), 1)
-            elif prediction == 'palmer':
+            elif prediction == 'yes':
                 draw(scale, col, row, step, (0,0,255), 1)
             col += step
         row += step
@@ -96,7 +121,7 @@ labels = []
 data = []
 # train a Linear SVM on the data
 #model = LinearSVC(C=100.0, random_state=42)
-model = LinearSVC(max_iter=5000)
+model = LinearSVC(max_iter=5000000)
 
 (h, w) = gray.shape[:2]
 print(h, w)
@@ -111,13 +136,20 @@ while row + step < h:
     row += step
 random.shuffle(work_list)
 
+count = 0
 for (row, col) in work_list:
     print(row, col)
-    lbp_region = lbp[row:row+step,col:col+step]
     rgb_region = rgb[row:row+step,col:col+step]
-    (hist, _) = np.histogram(lbp_region.ravel(),
-                             bins=np.arange(0, numPoints + 3),
-                             range=(0, numPoints + 2))
+    hist = gen_classifier(lbp, index, row, col, step)
+    if False:
+        # dist histogram
+        plt.figure()
+        y_pos = np.arange(len(hist))
+        plt.bar(y_pos, hist, align='center', alpha=0.5)
+        plt.xticks(y_pos, range(len(hist)))
+        plt.ylabel('count')
+        plt.title('classifier')
+        plt.show()
     scale_copy = scale.copy()
     draw(scale_copy, col, row, step, (255,255,255), 2)
     cv2.imshow('gray', gscale)
@@ -125,14 +157,13 @@ for (row, col) in work_list:
     cv2.imshow('region', cv2.resize(rgb_region, (step*2, step*2)))
     key = cv2.waitKey()
     if key == ord('y') or key == ord('Y'):
-        labels.append('palmer')
+        labels.append('yes')
         data.append(hist)
-        if len(set(labels)) >= 2:
-            model.fit(data, labels)
-            update_guess(gray, step, scale, model)
     elif key == ord('n') or key == ord('N'):
         labels.append('no')
         data.append(hist)
+    count += 1
+    if count % 10 == 0:
         if len(set(labels)) >= 2:
             model.fit(data, labels)
             update_guess(gray, step, scale, model)
