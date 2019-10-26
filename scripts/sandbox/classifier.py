0e540dab989ae9f7a870e63056a1ddb79faefaf4
Curtis L. Olson <curtolson@flightgear.org>
2019-10-26 11:11:31 -0500

    Initial revision of a classifier class.

---
+++ scripts/sandbox/classifier.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+
+# Machine learning module built with Linear Support Vectors
+
+import cv2
+import numpy as np
+import os
+import pickle
+import skimage                    # pip3 install scikit-image
+import sklearn.svm                # pip3 install scikit-learn
+
+class Learn():
+
+    index = None
+    model = None
+    cells = {}
+    saved_labels = []
+    saved_data = []
+
+    def __init__(self):
+        pass
+
+    def init_model(self, fitname=None, dataname=None):
+        if fitname and os.path.isfile(fitname):
+            print("Loading LinearSVC model from:", fitname)
+            self.model = pickle.load(open(fitname, "rb"))
+            #update_prediction(cell_list, model)
+        else:
+            print("Initializing  a new LinearSVC model")
+            self.model = LinearSVC(max_iter=5000000)
+        if dataname and os.path.isfile(dataname):
+            print("Loading saved model data from:", dataname)
+            (self.saved_labels, self.saved_data) = \
+                pickle.load( open(dataname, "rb"))
+
+    # compute the Local Binary Pattern representation of the image
+    def compute_lbp(self, image, radius=3):
+        print("Computing LBP")
+        self.radius = radius
+        self.numPoints = radius * 8
+        if len(image.shape) == 3:
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        else:
+            gray = image
+        self.index = skimage.feature.local_binary_pattern(gray,
+                                                          self.numPoints,
+                                                          self.radius,
+                                                          method="uniform")
+
+    # and then use the index representation (i.e. LBP) to build the
+    # histogram of values
+    def gen_classifier(self, r1, r2, c1, c2):
+        region = self.index[r1:r2,c1:c2]
+        (hist, _) = np.histogram(region.ravel(),
+                                 bins=np.arange(0, self.numPoints + 3),
+                                 range=(0, self.numPoints + 2))
+        # if texture_and_color:
+        #     index_region = index[r1:r2,c1:c2]
+        #     (index_hist, _) = np.histogram(index_region.ravel(),
+        #                                    bins=64,
+        #                                    range=(0, 255))
+        #     #index_hist[0] = 0
+        #     hist = np.concatenate((hist, index_hist), axis=None)
+        # if False:
+        #     # dist histogram
+        #     plt.figure()
+        #     y_pos = np.arange(len(hist))
+        #     plt.bar(y_pos, hist, align='center', alpha=0.5)
+        #     plt.xticks(y_pos, range(len(hist)))
+        #     plt.ylabel('count')
+        #     plt.title('classifier')
+        #     plt.show()
+        return hist
+
+    # compute the grid layout and classifier
+    def compute_grid(self, grid_size=160):
+        (h, w) = self.index.shape[:2]
+        hcells = int(h / grid_size)
+        wcells = int(w / grid_size)
+        self.rows = np.linspace(0, h, hcells).astype('int')
+        self.cols = np.linspace(0, w, wcells).astype('int')
+        self.cells = {}
+        for j in range(len(self.rows)-1):
+            for i in range(len(self.cols)-1):
+                (r1, r2, c1, c2) = (int(self.rows[j]), int(self.rows[j+1]),
+                                    int(self.cols[i]), int(self.cols[i+1]))
+                key = "%d,%d,%d,%d" % (r1, r2, c1, c2)
+                self.cells[key] = { "region": (r1, r2, c1, c2),
+                                    "classifier": None,
+                                    "user": None,
+                                    "prediction": [ '0' ] }
+                                    
+        for key in self.cells:
+            (r1, r2, c1, c2) = self.cells[key]["region"]
+            self.cells[key]["classifier"] = self.gen_classifier(r1, r2, c1, c2)
+
+    # do the model fit
+    def update_model(self, fitname=None, dataname=None):
+        labels = list(self.saved_labels)
+        data = list(self.saved_data)
+        for key in self.cells:
+            cell = self.cells[key]
+            if cell["user"] != None:
+                labels.append(cell["user"])
+                data.append(cell["classifier"])
+        if len(set(labels)) >= 2:
+            print("Updating model fit, training points:", len(data))
+            self.model.fit(data, labels)
+            if dataname:
+                print("Saving data:", dataname)
+                pickle.dump( (labels, data), open(dataname, "wb"))
+            if fitname:
+                print("Saving model:", fitname)
+                pickle.dump(self.model, open(fitname, "wb"))
+            print("Done.")
+ 
+    def update_prediction(self):
+        if self.model == None:
+            print("No model defined in update_prediction()")
+            return
+        for key in self.cells:
+            cell = self.cells[key]
+            cell["prediction"] = \
+                self.model.predict(cell["classifier"].reshape(1, -1))
+
+    # return the key of the cell containing the give x, y pixel coordinate
+    def find_key(self, x, y):
+        i = np.searchsorted(self.cols, x, side='right') - 1
+        j = np.searchsorted(self.rows, y, side='right') - 1
+        key = "%d,%d,%d,%d" % (int(self.rows[j]), int(self.rows[j+1]),
+                               int(self.cols[i]), int(self.cols[i+1]))
+        return key
