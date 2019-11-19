234b872c3472a96a70be807274b04d691af618a3
Curtis L. Olson <olson126@umn.edu>
2019-11-19 14:20:21 -0600

    skvideo support.

--- scripts/sandbox/range_detector-rg.py
+++ scripts/sandbox/range_detector-rg.py
@@ -11,6 +11,7 @@ import cv2
 import numpy as np
 import argparse
 from operator import xor
+import skvideo.io               # pip3 install sk-video
 
 
 def callback(value):
@@ -35,13 +36,14 @@ def get_arguments():
                     help='Path to the image')
     ap.add_argument('-w', '--webcam', required=False,
                     help='Use webcam', action='store_true')
+    ap.add_argument('--video', help='Specify a video as input')
     ap.add_argument('-p', '--preview', required=False,
                     help='Show a preview of the image after applying the mask',
                     action='store_true')
     args = vars(ap.parse_args())
 
-    if not xor(bool(args['image']), bool(args['webcam'])):
-        ap.error("Please specify only one image source")
+    #if not xor(bool(args['image']), bool(args['webcam'])):
+    #    ap.error("Please specify only one image source")
 
     if not args['filter'].upper() in ['RGB', 'HSV', 'GGG']:
         ap.error("Please speciy a correct filter.")
@@ -90,13 +92,31 @@ def main():
             index = (ratio*255).astype('uint8')
             frame_to_thresh = cv2.merge( (index, index, index) )
 
+    elif args['video']:
+        print("Opening ", args['video'])
+        reader = skvideo.io.FFmpegReader(args['video'], inputdict={}, outputdict={})
     else:
         camera = cv2.VideoCapture(0)
 
     setup_trackbars(range_filter)
 
     while True:
-        if args['webcam']:
+        if args['video']:
+            for image in reader.nextFrame():
+                image = image[:,:,::-1]     # convert from RGB to BGR (to make opencv happy)
+
+                #frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+                frame_to_thresh = image.copy()
+                h, s, v = cv2.split(frame_to_thresh)
+                cv2.imshow('value', v)
+                print(np.average(v))
+                v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+                thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+                preview = cv2.bitwise_and(image, image, mask=thresh)
+                cv2.imshow("Preview", preview)
+                if cv2.waitKey(1) & 0xFF is ord('q'):
+                    break
+        elif args['webcam']:
             ret, image = camera.read()
 
             if not ret:
