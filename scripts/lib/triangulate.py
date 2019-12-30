5ecc9f0fea43395c73c9e3b51838f012909739f6
Curtis L. Olson <olson126@umn.edu>
2019-12-30 09:12:38 -0600

    Separate out a function to 3d triangulate match pairs from camera poses.

---
+++ scripts/lib/triangulate.py
@@ -0,0 +1,66 @@
+# code to trianglulate image features (in 3d) based on camera poses
+
+import cv2
+import numpy as np
+
+from . import camera
+from . import image
+from . import project
+
+# compute the 3d triangulation of the matches between a pair of images
+def triangulate_ned(i1, i2):
+    # quick sanity checks
+    if i1 == i2:
+        return None
+    if not i2.name in i1.match_list:
+        return None
+    if len(i1.match_list[i2.name]) == 0:
+        return None
+
+    if not i1.kp_list or not len(i1.kp_list):
+        i1.load_features()
+    if not i2.kp_list or not len(i2.kp_list):
+        i2.load_features()
+
+    # camera calibration
+    K = camera.get_K()
+    IK = np.linalg.inv(K)
+
+    # get poses
+    rvec1, tvec1 = i1.get_proj()
+    rvec2, tvec2 = i2.get_proj()
+    R1, jac = cv2.Rodrigues(rvec1)
+    PROJ1 = np.concatenate((R1, tvec1), axis=1)
+    R2, jac = cv2.Rodrigues(rvec2)
+    PROJ2 = np.concatenate((R2, tvec2), axis=1)
+
+    # setup data structurs of cv2 call
+    uv1 = []; uv2 = []; indices = []
+    for pair in i1.match_list[i2.name]:
+        p1 = i1.kp_list[ pair[0] ].pt
+        p2 = i2.kp_list[ pair[1] ].pt
+        uv1.append( [p1[0], p1[1], 1.0] )
+        uv2.append( [p2[0], p2[1], 1.0] )
+    pts1 = IK.dot(np.array(uv1).T)
+    pts2 = IK.dot(np.array(uv2).T)
+    points = cv2.triangulatePoints(PROJ1, PROJ2, pts1[:2], pts2[:2])
+    points /= points[3]
+    return points
+
+def estimate_surface(i1, i2):
+    points = triangulate(i1, i2)
+    if points is None:
+        return None
+    else:
+        return np.average(points[2])*num_matches
+    
+#     num_matches = len(i1.match_list[i2.name])
+#     sum += np.average(points[2])*num_matches
+#     count += num_matches
+
+#     print(" ", i1.name, "+", i2.name, "avg ground est: %.1f" % np.average(points[2]), "srtm: %.1f" % ((g1 + g2)*0.5) )
+    
+# if count > 0:
+#     print(i1.name, "estimated surface below:", "%.1f" % (sum / count))
+# else:
+#     print(i1.name, "no matches, no triangulation, no estimate")
