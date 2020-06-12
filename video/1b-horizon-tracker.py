9356fbb2dc5df3b072e1370921b3779b5ffbe509
Curtis L. Olson <curtolson@flightgear.org>
2020-06-11 20:58:44 -0500

    Apply edge detection to entire blue channel. Apply otsu threshold mask to the edges after being detected, this way we aren't just finding the edge of the mask.  Masking off extraneous edges then makes the houghs transform run faster. There is room for more refinement, but pretty happy with the overall performance across several challenging scenarios.

--- video/1b-horizon-tracker.py
+++ video/1b-horizon-tracker.py
@@ -111,29 +111,41 @@ def horizon(frame):
     print("horizon")
     # attempt to threshold on high blue values (blue<->white)
     b, g, r = cv2.split(frame)
-    #cv2.imshow("b", b)
+    cv2.imshow("b", b)
     #cv2.imshow("g", g)
     #cv2.imshow("r", r)
     print("shape:", frame.shape)
     #print("blue range:", np.min(b), np.max(b))
     #print('ave:', np.average(b), np.average(g), np.average(r))
 
-    if True:
-        # Otsu thresholding
-        ret2, thresh = cv2.threshold(b, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        print('ret2:', ret2)
-        cv2.imshow('otsu mask', thresh)
-    else:
-        # global thresholding
-        thresh = cv2.inRange(frame, (210, 0, 0), (255, 255, 255))
-        cv2.imshow('global mask', thresh)
+    # Otsu thresholding on blue channel
+    ret2, thresh = cv2.threshold(b, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    print('ret2:', ret2)
+    cv2.imshow('otsu mask', thresh)
+
+    # dilate the mask a small bit
+    kernel = np.ones((5,5), np.uint8)
+    thresh = cv2.dilate(thresh, kernel, iterations=1)
+  
+    # global thresholding on blue channel before edge detection
+    #thresh = cv2.inRange(frame, (210, 0, 0), (255, 255, 255))
+    #cv2.imshow('global mask', thresh)
         
     preview = cv2.bitwise_and(frame, frame, mask=thresh)
     cv2.imshow("threshold", preview)
-    
-    # 200, 600
-    edges = cv2.Canny(thresh, 200, 600)
+
+    # the lower the 1st canny number the more total edges are accepted
+    # the lower the 2nd canny number the less hard the edges need to
+    # be to accept an edge
+    edges = cv2.Canny(b, 50, 150)
+    #edges = cv2.Canny(b, 200, 600)
     cv2.imshow("edges", edges)
+
+    # Use the blue mask (Otsu) to filter out edge noise in area we
+    # don't care about (to improve performance of the hough transform)
+    edges = cv2.bitwise_and(edges, edges, mask=thresh)
+    cv2.imshow("masked edges", edges)
+    
     #theta_res = np.pi/180       # 1 degree
     theta_res = np.pi/1800      # 0.1 degree
     threshold = int(frame.shape[1] / 8)
@@ -167,9 +179,10 @@ for frame in reader.nextFrame():
     filtered = []
 
     if counter < skip_frames:
-        if counter % 1000 == 0:
+        if counter % 100 == 0:
             print("Skipping %d frames..." % counter)
-        continue
+        else:
+            continue
 
     print("Frame %d" % counter)
 
