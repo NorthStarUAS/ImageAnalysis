6a55c48fa5d6c4aad6ba6fffd7b00eebee8bbdd5
Curtis L. Olson <curtolson@flightgear.org>
2021-02-25 07:58:05 -0600

    Move some horizon project code here.

--- video/camera.py
+++ video/camera.py
@@ -164,3 +164,76 @@ class VirtualCamera:
         else:
             return None
 
+
+    # utility functions to support various efforts
+
+    # precompute to save time
+    horiz_ned = [0, 0, 0]  # any value works here (as long as it's consistent
+    horiz_divs = 10
+    horiz_pts = []
+    for i in range(horiz_divs + 1):
+        a = (float(i) * 360/float(horiz_divs)) * d2r
+        n = math.cos(a) + horiz_ned[0]
+        e = math.sin(a) + horiz_ned[1]
+        d = 0.0 + horiz_ned[2]
+        horiz_pts.append( [n, e, d] )
+
+    # returns roll, pitch of horizon given current camera attitude.
+    # Note camera attitude typically includes aircraft body attitude
+    # and camera mount offset.  (The attiude values and offsets should
+    # be set/updated by the calling function)
+    def find_horizon(self):
+        answers = []
+        K = self.get_K()
+        IK = self.get_IK()
+        cu = K[0,2]
+        cv = K[1,2]
+        for i in range(self.horiz_divs):
+            uv1 = self.project_ned( self.horiz_pts[i] )
+            uv2 = self.project_ned( self.horiz_pts[i+1] )
+            if uv1 != None and uv2 != None:
+                #print(" ", uv1, uv2)
+                roll, pitch = self.get_projected_attitude(uv1, uv2, IK, cu, cv)
+                answers.append( (roll, pitch) )
+        if len(answers) > 0:
+            index = int(len(answers) / 2)
+            return answers[index]
+        else:
+            return None, None
+            
+    # a, b are line end points, p is some other point
+    # returns the closest point on ab to p (orthogonal projection)
+    def ClosestPointOnLine(self, a, b, p):
+        ap = p - a
+        ab = b - a
+        return a + np.dot(ap,ab) / np.dot(ab,ab) * ab
+
+    # get the roll/pitch of camera orientation relative to specified
+    # horizon line
+    def get_projected_attitude(self, uv1, uv2, IK, cu, cv):
+        # print('line:', line)
+        du = uv2[0] - uv1[0]
+        dv = uv1[1] - uv2[1]        # account for (0,0) at top left corner in image space
+        roll = math.atan2(dv, du)
+
+        if False:
+            # temp test
+            w = cu * 2
+            h = cv * 2
+            for p in [ (0, 0, 1), (w, 0, 1), (0, h, 1), (w, h, 1), (cu, cv, 1) ]:
+                uvh = np.array(p)
+                proj = IK.dot(uvh)
+                print(p, "->", proj)
+
+        p0 = self.ClosestPointOnLine(np.array(uv1), np.array(uv2),
+                                     np.array([cu,cv]))
+        uvh = np.array([p0[0], p0[1], 1.0])
+        proj = IK.dot(uvh)
+        #print("proj:", proj, proj/np.linalg.norm(proj))
+        dot_product = np.dot(np.array([0,0,1]), proj/np.linalg.norm(proj))
+        pitch = np.arccos(dot_product)
+        if p0[1] < cv:
+            pitch = -pitch
+        #print("roll: %.1f pitch: %.1f" % (roll, pitch))
+        return roll, pitch
+
